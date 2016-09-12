c86fca44b4b8b9948963272cc8853be9292d6bd4
cch <christian.henkel@ipa.fraunhofer.de>
2016-09-12 10:40:11 +0200

    trying another viz

--- smartleitstand/test_qt5.py
+++ smartleitstand/test_qt5.py
@@ -1,64 +1,42 @@
+# simple code by Krystian Samp - krychu (samp[dot]krystian[monkey]gmail.com), November 2006
 
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+import sys
+from PyQt4 import QtGui, QtCore
 
-"""
-ZetCode PyQt5 tutorial
-
-In the example, we draw randomly 1000 red points
-on the window.
-
-author: Jan Bodnar
-website: zetcode.com
-last edited: January 2015
-"""
-
-import sys, random, time, threading
-from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-
-class Example(QWidget):
-
+class MyView(QtGui.QGraphicsView):
     def __init__(self):
-        super().__init__()
+        QtGui.QGraphicsView.__init__(self)
 
-        self.initUI()
+        self.scene = QtGui.QGraphicsScene(self)
+        self.item = QtGui.QGraphicsEllipseItem(1000, 0, 10, 10)
+        color = QtGui.QColor(10, 200, 10, 127)
+        brush = QtGui.QBrush(color)
+        self.item.setBrush(brush)
+        self.scene.addItem(self.item)
+        self.setScene(self.scene)
+        self.resize(130, 130)
 
+        # Remember to hold the references to QTimeLine and QGraphicsItemAnimation instances.
+        # They are not kept anywhere, even if you invoke QTimeLine.start().
+        self.tl = QtCore.QTimeLine(1000)
+        self.tl.setFrameRange(0, 100)
+        self.a = QtGui.QGraphicsItemAnimation()
+        self.a.setItem(self.item)
+        self.a.setTimeLine(self.tl)
 
-    def initUI(self):
+        # Each method determining an animation state (e.g. setPosAt, setRotationAt etc.)
+        # takes as a first argument a step which is a value between 0 (the beginning of the
+        # animation) and 1 (the end of the animation)
+        self.a.setPosAt(0, QtCore.QPointF(0, 0))
+        self.a.setPosAt(.25, QtCore.QPointF(0, 100))
+        self.a.setPosAt(.5, QtCore.QPointF(100, 100))
+        self.a.setPosAt(.75, QtCore.QPointF(100, 0))
+        self.a.setPosAt(1, QtCore.QPointF(50, 50))
 
-        self.setGeometry(0, 0, 1000, 1000)
-        self.setWindowTitle('Points')
-        self.show()
-
-
-
-
-    def paintEvent(self, e):
-        print("Paint event ..")
-        qp = QPainter()
-        qp.begin(self)
-        self.drawPoints(qp)
-        qp.end()
-
-    def drawPoints(self, qp):
-
-        qp.setPen(Qt.darkBlue)
-        size = self.size()
-
-        for i in range(1000):
-            x = random.randint(1, size.width()-1)
-            y = random.randint(1, size.height()-1)
-            qp.drawEllipse(QPointF(x,y), 3, 3)
-
+        self.tl.start()
 
 if __name__ == '__main__':
-
-    app = QApplication(sys.argv)
-    ex = Example()
-
-    t = threading.Timer(20 * 60, ex.paintEvent(ex, QEvent()))
-    t.start()
-
+    app = QtGui.QApplication(sys.argv)
+    view = MyView()
+    view.show()
     sys.exit(app.exec_())
