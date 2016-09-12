0147bd38da7b392fce516534412d79f1a20715ab
cch <christian.henkel@ipa.fraunhofer.de>
2016-09-12 11:16:55 +0200

    restructuring and msb connection

---
+++ smartleitstand/smartleitstand.py
@@ -0,0 +1,7 @@
+def which_car(cars, routeTodo, routesQueue, ):
+    print("### SMARTLEITSTAND ###")
+    print("cars:" + str(cars))
+    print("routeTodo:" + routeTodo.to_string())
+    print("routesQueue:" + str(routesQueue))
+    print("######################")
+    return cars[0]
