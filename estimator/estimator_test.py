b7dfeaaf121d18a0babdc3d2283a47a05228257a
cch <christian.henkel@ipa.fraunhofer.de>
2016-11-30 10:30:33 +0100

    an algorithm generating simple demo data for testing

--- estimator/estimator_test.py
+++ estimator/estimator_test.py
@@ -1,4 +1,55 @@
+import numpy as np
+from datetime import datetime
+
 import estimator as e
+
+
+def generate_data():
+    """
+    generating example data that simulates a number of
+    agvs
+    """
+    nr_agents = 5
+    duration_landmarks_mean = np.arange(4, 12)
+    duration_landmarks_std = 4
+    nr_landmarks = len(duration_landmarks_mean)
+    duration_travel = 15
+    trips_todo = 10000
+    timestep = .5
+
+    location = np.zeros(nr_agents)
+    timeout = np.zeros(nr_agents)
+    history = []
+    t = 0
+    while len(history) < trips_todo:
+
+        at_timeout = timeout <= 0
+        at_landmark = location % 1 == 0
+
+        # update agents departing from stations
+        for agent in np.arange(nr_agents)[at_landmark & at_timeout]:
+            current_landmark = location[agent]
+            next_landmark = (current_landmark + 1) % nr_landmarks
+
+            location[agent] = current_landmark + 0.5
+            timeout[agent] = duration_travel
+
+            history.append([t, current_landmark, next_landmark])
+
+        # update agents arriving at stations
+        for agent in np.arange(nr_agents)[np.invert(at_landmark) & at_timeout]:
+            arriving_landmark = np.ceil(location[agent]) % nr_landmarks
+
+            location[agent] = arriving_landmark
+            timeout[agent] = np.random.normal(
+                loc=duration_landmarks_mean[agent],
+                scale=duration_landmarks_std
+            )
+
+        t += timestep
+        timeout -= timestep
+
+    return history
 
 
 def base_test():
@@ -6,3 +57,11 @@ def base_test():
     e.update(s, 0, 4, 5)
 
     assert e.estimation(s, 4, 5) == (1, 0), "with only one update we expect a clear result"
+
+
+if __name__ == "__main__":
+    start = datetime.now()
+    history = generate_data()
+    print("Generated", len(history), "samples")
+    print(history[-5:])
+    print("Computation took", str((datetime.now()-start).total_seconds()), "s")
