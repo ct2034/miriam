dde3bca5852e1be05cd2eb8269b609989d7754fc
cch <christian.henkel@ipa.fraunhofer.de>
2016-12-01 17:40:34 +0100

    smooth plot

--- estimator/estimator.py
+++ estimator/estimator.py
@@ -63,12 +63,46 @@ def update_list(_s: state, l: list):
     return _s
 
 
-def info(_s):
+def savitzky_golay(y, window_size, order, deriv=0, rate=1):
+    """
+    src: https://stackoverflow.com/questions/22988882/how-to-smooth-a-curve-in-python
+    """
+    import numpy as np
+    from math import factorial
+
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError as msg:
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
+    # precompute coefficients
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve( m[::-1], y, mode='valid')
+
+
+def info(_s: state):
     """
     Print info about current state
     :param _s: the state
     """
-    plt.plot(np.array(_s.testing))
+    #smooth
+    smooth = np.array(_s.testing)
+    for i in range(_s.nr_landmarks):
+        smooth[:, i] = savitzky_golay(np.array(_s.testing)[:, i], 19, 5)
+
+    plt.plot(smooth)
     plt.legend(_s.legend)
     plt.show()
 
