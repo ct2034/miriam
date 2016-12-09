af7184c1d7f7d6cecef2861a90e9cd001d277a18
cch <christian.henkel@ipa.fraunhofer.de>
2016-12-09 11:12:49 +0100

    comparable demo of networkx!

--- astar/networkx_demo.py
+++ astar/networkx_demo.py
@@ -17,13 +17,27 @@ map[20:, 80] = -1
 start = (10, 10)
 goal = (90, 90)
 
-
 def cost(a, b):
     if map[a] >= 0 and map[b] >= 0:  # no obstacle
         return astar_grid8con.cost(a, b, map)
     else:
         return np.Inf
 
+
+obstacle = []
+for n in G.nodes_iter():
+    if not map[n] >= 0:  # obstacle
+        obstacle.append(n)
+
+G.remove_nodes_from(obstacle)
+
+for n in G.nodes_iter():
+    for d in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+        checking = (d[0] + n[0], d[1] + n[1])
+        if G.has_node(checking):
+            if not G.has_edge(n, checking):
+                G.add_edge(n, checking)
+            G[n][checking]['weight'] = cost(n, checking)
 
 t = timeit.Timer()
 t.timeit()
