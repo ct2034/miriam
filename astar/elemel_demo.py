7ad15c8f70cf812bdb7ca6aae7dcd0cb6469e62e
cch <christian.henkel@ipa.fraunhofer.de>
2016-12-11 19:09:12 +0100

    make it python3

--- astar/elemel_demo.py
+++ astar/elemel_demo.py
@@ -6,14 +6,14 @@ import numpy as np
 
 from astar import astar_grid48con
 
-grid = np.zeros([100, 100])
-grid[:80, 20] = -1
-grid[80, 20:60] = -1
-grid[20, 40:80] = -1
-grid[20:, 80] = -1
+grid = np.zeros([100, 100, 100])
+grid[:80, 20, :] = -1
+grid[80, 20:60, :] = -1
+grid[20, 40:80, :] = -1
+grid[20:, 80, :] = -1
 
-start = (10, 10)
-goal = (90, 90)
+start = (10, 10, 0)
+goal = (90, 90, 99)
 
 t = timeit.Timer()
 t.timeit()
@@ -21,8 +21,8 @@ path = elemel_astar.src.astar.astar(start_pos=start,
                                     neighbors=lambda pos: astar_grid48con.get_children(pos, grid),
                                     goal=lambda pos: pos == goal,
                                     start_g=0,
-                                    cost=lambda a, b: astar_grid48con.cost(a, b, grid),
-                                    heuristic=lambda pos: astar_grid48con.heuristic(pos, goal, grid)
+                                    cost=lambda a, b: astar_grid48con.cost(a, b, False),
+                                    heuristic=lambda pos: astar_grid48con.heuristic(pos, goal, False)
                                     )
 print("computation time:", t.repeat(), "s")
 
@@ -33,7 +33,7 @@ print("length: ", astar_grid48con.path_length(path))
 
 fig, ax = plt.subplots()
 
-ax.imshow(grid.T, cmap='Greys', interpolation='nearest')
+# ax.imshow(grid.T, cmap='Greys', interpolation='nearest')
 ax.set_title('astar path')
 ax.axis([0, grid.shape[0], 0, grid.shape[1]])
 ax.plot(
