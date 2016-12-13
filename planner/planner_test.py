09748cb22fe2093c0feb24942ebfa8dfdbfd744b
cch <christian.henkel@ipa.fraunhofer.de>
2016-12-13 15:34:12 +0100

    testing

--- planner/planner_test.py
+++ planner/planner_test.py
@@ -1,10 +1,11 @@
+import datetime
 import numpy as np
 import planner.plan
 
 
 def test_basic():
-    grid = np.zeros([10, 10])
-    grid[4, 2:8] = -1
+    grid = np.zeros([10, 10, 51])
+    grid[4, 2:8, :] = -1
 
     # input
     agent_pos = [(1, 1), (9, 1), (3, 1)]  # three agents
@@ -12,10 +13,14 @@ def test_basic():
     idle_goals = [((9, 7), (5, .5))]  # one idle goal 9,7 with P~N(5,.5)
 
     # expected results
-    agent_job = [(0, 0)]
-    agent_idle = [(1, 0)]
+    agent_job = ((0, 0), (2, 1))
+    agent_idle = ((1, 0),)
 
-    res_agent_job, res_agent_idle = planner.plan.plan(agent_pos, jobs, idle_goals, grid)
+    start_time = datetime.datetime.now()
+
+    res_agent_job, res_agent_idle, res_blocked = planner.plan.plan(agent_pos, jobs, idle_goals, grid)
+
+    print("computation time:", (datetime.datetime.now() - start_time).total_seconds(), "s")
 
     assert res_agent_job == agent_job, "wrong agent -> job assignment"
     assert res_agent_idle == agent_idle, "wrong agent -> idle_goal assignment"
