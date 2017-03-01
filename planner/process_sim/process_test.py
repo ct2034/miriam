ba87b1996c714c9d7257959d4302441dd424751a
cch <christian.henkel@ipa.fraunhofer.de>
2017-03-01 01:10:35 +0100

    sonar qube?

--- planner/process_sim/process_test.py
+++ planner/process_sim/process_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import logging
-import threading
 from datetime import datetime
 import time
 import numpy as np
@@ -23,42 +22,52 @@ stations = [[0, 0],
             [9, 4]]
 
 flow = [[0, 2],
-        [1, 3],
-        [2, 1],
-        [4, 2],
-        [3, 3],
-        [5, 3],
+        # [1, 3],
+        # [2, 1],
+        # [4, 2],
+        # [3, 3],
+        # [5, 3],
         [6, 2]
         ]
 
-products_todo = 3
-state = np.zeros(products_todo)
-t_left = np.zeros(products_todo)
-
+products_todo = 1
 t_step = .1
 
 
 def run(agv_sim):
+    print("START")
+    state = np.zeros(products_todo)
+    t_left = np.zeros(products_todo)
+    t_start = datetime.now()
     products_finished = 0
     while products_finished < products_todo:
         for p in range(products_todo):
             if state[p] % 1 == .5:  # in transport
-                if agv_sim.is_finished(p):
+                if agv_sim.is_finished(hash(p + 100 * (state[p] - .5))):
                     state[p] += .5
                     t_left[p] = flow[int(state[p])][1]
+                    print("PRODUCT %d in state %1.1f" % (p + 1, state[p]))
             else:  # in station
                 if state[p] == len(flow) - 1:  # finished
                     state[p] = len(flow)
                     products_finished += 1
+                    print("PRODUCT %d FINISHED" % (p + 1))
                 elif state[p] < len(flow) - 1:  # running
                     if t_left[p] <= 0:
                         agv_sim.new_job(np.array(stations[flow[int(state[p])][0]], dtype=int),
                                         np.array(stations[flow[int(state[p]) + 1][0]], dtype=int),
-                                        p)
+                                        hash(p + 100 * state[p]))
                         state[p] += .5
+                        print("PRODUCT %d in state %1f" % (p + 1, state[p]))
                     else:
                         t_left[p] -= t_step
         time.sleep(t_step)
+
+    t_finished = datetime.now()
+    print("ALL PRODUCTS FINISHED")
+    t_dur = (t_finished - t_start).total_seconds()
+    print("Took %.3f s" % t_dur)
+    return t_dur
 
 
 _map = np.zeros([10, 10, 51])
@@ -80,9 +89,8 @@ def run_with_module(mod):
     agv_sim = SimpSim(False, mod)
     agv_sim.start()
     agv_sim.start_sim(20, 20, 2)
-    start_object = run(agv_sim)
-    n = start_object.start(3, agv_sim)
-    agv_sim.stop()
+    n = run(agv_sim)
+    agv_sim.stop_sim()
     return n
 
 
