63c518d8869d917782f9418134c2461c6be7a8c7
cch <christian.henkel@ipa.fraunhofer.de>
2017-06-16 17:25:51 +0200

    teschd

--- logistics_controller/src/robot_controller_test.py
+++ logistics_controller/src/robot_controller_test.py
@@ -1,18 +1,18 @@
 #!/bin/env python2
-import robot_controller
+import logistics_controller as rc
 
 
 def conversion_plan_map_test_a():
-    assert (-2, -2) == robot_controller.plan_to_map((5, 3))
+    assert (-2, -2) == rc.plan_to_map((5, 3))
 
 
 def conversion_plan_map_test_b():
-    assert (8, -8) == robot_controller.plan_to_map((10, 0))
+    assert (8, -8) == rc.plan_to_map((10, 0))
 
 
 def conversion_map_plan_test_a():
-    assert (5, 3) == robot_controller.map_to_plan((-2, -2))
+    assert (5, 3) == rc.map_to_plan((-2, -2))
 
 
 def conversion_map_plan_test_b():
-    assert (10, 0) == robot_controller.map_to_plan((8, -8))
+    assert (10, 0) == rc.map_to_plan((8, -8))
