dc180071f6a95b5742969ee48a99b364864c0d45
cch <christian.henkel@ipa.fraunhofer.de>
2017-06-30 09:35:22 +0200

    A bigger problem, two

--- planner/cbs_ext/pathplanner_cbs_demo.py
+++ planner/cbs_ext/pathplanner_cbs_demo.py
@@ -5,17 +5,31 @@ import numpy as np
 from planner.cbs_ext.plan import plan
 
 grid = np.zeros([10, 10, 51])
-grid[4, 2:8, :] = -1
+grid[4, 1:9, :] = -1
 
-# input
-agent_pos = [(1, 1), (9, 1), (3, 1)]  # three agents
-jobs = [((1, 6), (9, 6)), ((3, 3), (7, 3)), ((1, 1),)]  # two jobs 1,6 -> 9,1, 3,3 -> 7,3, a simple goal: 1,1
 
-start_time = datetime.datetime.now()
+def pathplan():
+    start_time = datetime.datetime.now()
+    # This misuses the cbsext planner as cbs only planner by fixing the assignment
+    res_agent_job, res_agent_idle, res_paths = plan(agent_pos, jobs, [], [], grid,
+                                                    plot=False, filename='pathplanning_only.pkl',
+                                                    pathplanning_only=True)
+    print("computation time:", (datetime.datetime.now() - start_time).total_seconds(), "s")
+    for p in res_paths:
+        print(p)
 
-# This misuses the cbsext planner as cbs only planner by fixing the assignment
-res_agent_job, res_agent_idle, res_paths = plan(agent_pos, jobs, [], [], grid,
-                                                plot=True, filename=None, pathplanning_only=True)
 
-print("computation time:", (datetime.datetime.now() - start_time).total_seconds(), "s")
-print(res_agent_job, res_agent_idle, res_paths)
+# input 1
+agent_pos = [(1, 1), (2, 1), (3, 1)]  # three agents
+jobs = [((1, 7), (9, 1)), ((1, 8), (8, 1)), ((9, 8),)]  # two jobs 1,6 -> 9,1, 3,3 -> 7,3, a simple goal: 1,1
+
+pathplan()
+
+# input 2
+agent_pos = [(1, 1), (2, 1), (3, 1),
+             (6, 2), (7, 2), (7, 1)]
+
+jobs = [((1, 7), (9, 1)), ((1, 8), (8, 1)), ((9, 8),),
+        ((1, 1), (8, 3)), ((2, 1), (8, 7)), ((1, 2), (9, 9))]
+
+pathplan()
