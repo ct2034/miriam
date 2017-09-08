07e26adfc64eca92f1caef4215a94568d6379352
cch <christian.henkel@ipa.fraunhofer.de>
2017-09-08 17:23:47 +0200

    writing a file

--- planner/cobra/funwithsnakes.py
+++ planner/cobra/funwithsnakes.py
@@ -1,12 +1,65 @@
 import os
+import uuid
+from itertools import product
+
 import numpy as np
 
+MAP_EXT = ".map"
+TASK_EXT = ".map"
+PATH_EXT = ".map"
 
-def read_file(fname, grid):
+
+def plan_cobra(agent_pos, jobs, grid, config):
+    agent_job = []
+    cobra_filename_base = str(uuid.uuid1())
+    cobrabin = os.getenv("COBRA_BIN")
+
+    write_map_file(agent_pos, jobs, grid, cobra_filename_base)
+
+    os.system("pwd")
+    res = os.system(" ".join([
+        cobrabin,
+        cobra_filename_base + MAP_EXT,
+        cobra_filename_base + TASK_EXT
+    ]))
+    assert res == 0, "Error when calling cobra"
+
+    paths = read_path_file(cobra_filename_base + PATH_EXT, grid)
+    return agent_job, paths
+
+
+def write_map_file(agent_pos, jobs, grid, cobra_filename_base):
+    fname = cobra_filename_base + MAP_EXT
+    job_endpoints = set()
+    for j in jobs:
+        job_endpoints.add(j[0])
+        job_endpoints.add(j[1])
+    agent_points = set()
+    for a in agent_pos:
+        agent_points.add(a)
+
+    with open(fname, 'w') as f:
+        f.write(",".join(map(str, grid.shape[0:2])) + "\n")
+        f.write(str(len(job_endpoints)) + "\n")
+        f.write(str(len(agent_points)) + "\n")
+        for x in range(grid.shape[0]):
+            line = ""
+            for y in range(grid.shape[1]):
+                if grid[x][y][0] != 0:
+                    line += "@"
+                elif (x, y) in job_endpoints:
+                    line += "e"
+                elif (x, y) in agent_points:
+                    line += "r"
+                else:
+                    line += "."
+            f.write(line + "\n")
+
+    f.close()
+
+
+def read_path_file(fname, grid):
     height = grid.shape[1]
-    path = os.getenv("COBRA_PATH")
-    if path:
-        fname = path + "/" + fname
     paths = []
     agent_path = []
     t = 0
--- planner/cobra/funwithsnakes_test.py
+++ planner/cobra/funwithsnakes_test.py
@@ -2,13 +2,14 @@ import os
 
 import numpy as np
 
-from planner.cobra.funwithsnakes import read_file
+from planner.cbs_ext.plan import generate_config
+from planner.cobra.funwithsnakes import read_path_file, plan_cobra
 
 
-def test_read_file(fname='planner/cobra/test.path', plot=False):
+def test_read_map(fname='planner/cobra/test.path', plot=False):
     print("cwd: " + str(os.getcwd()))
     grid = np.zeros([10, 10, 100])
-    paths = read_file(fname, grid)
+    paths = read_path_file(fname, grid)
     if plot:
         import matplotlib.pyplot as plt
         from planner.cbs_ext.plan import plot_results
@@ -17,5 +18,16 @@ def test_read_file(fname='planner/cobra/test.path', plot=False):
     assert len(paths[0][0]) == 30, "No full paths"
 
 
+def test_cobra_simple():
+    res_agent_job, res_paths = plan_cobra(
+        [(1, 1), (2, 2)],
+        [((3, 3), (1, 4), 0), ((4, 1), (0, 0), 0)],
+        np.zeros([5, 5, 100]),
+        generate_config()
+    )
+    assert res_agent_job, "No result"
+    assert res_paths, "No result"
+
+
 if __name__ == "__main__":
-    test_read_file("/home/cch/src/cobra/COBRA/small_collision.task_tp_path", True)
+    test_cobra_simple()
