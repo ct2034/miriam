f5e177791245f2c5e21c68f1784b3b3819c44b7d
cch <christian.henkel@ipa.fraunhofer.de>
2017-09-15 17:46:49 +0200

    3D

---
+++ planner/matplotlib_3d_demo.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from itertools import product
+
+from tools import load_map
+
+_ = Axes3D
+plt.style.use('bmh')
+
+grid = load_map('map.png')
+grid = np.repeat(grid[:, :, np.newaxis], 100, axis=2)
+grid = np.swapaxes(grid, 0, 1)
+
+f = plt.figure()
+ax = f.add_subplot(111, projection='3d')
+ax.set_xlim3d(0, grid.shape[0])
+ax.set_ylim3d(0, grid.shape[1])
+ax.set_zlim3d(0, grid.shape[2])
+
+xx, yy = np.meshgrid(
+    np.linspace(-.5, grid.shape[0] + .5, grid.shape[0] * 100),
+    np.linspace(-.5, grid.shape[1] + .5, grid.shape[1] * 100))
+
+img = np.zeros([xx.shape[0], yy.shape[1]])
+
+for x, y in product(range(len(xx[0, :])), range(len(yy[:, 0]))):
+    try:
+        img[x, y] = grid[
+                        int(round(xx[0, x] - 1)),
+                        int(round(yy[y, 0] - 1)),
+                        0] * -.001
+    except IndexError:
+        pass
+
+plt.contourf(xx, yy, img, origin=None, antialiased=False, nchunks=0, cmap=cm.Greys)
+
+plt.show()
