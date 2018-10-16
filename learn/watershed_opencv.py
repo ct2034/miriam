fc1ead842b4219596e0dce5b82b035ec11db5df2
ct2034 <post@henkelchristian.de>
2018-10-16 15:45:53 +0200

    actual watershed (!)

--- learn/watershed_opencv.py
+++ learn/watershed_opencv.py
@@ -1,43 +1,69 @@
+#### source: https://www.pyimagesearch.com/2015/11/02/watershed-opencv/
 # import the necessary packages
-from __future__ import print_function
 from skimage.feature import peak_local_max
 from skimage.morphology import watershed
 from scipy import ndimage
+import numpy as np
 import argparse
 import cv2
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to input image")
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
 args = vars(ap.parse_args())
 
 # load the image and perform pyramid mean shift filtering
 # to aid the thresholding step
 image = cv2.imread(args["image"])
+image = cv2.resize(image, (0,0), fx=3, fy=3)
 shifted = cv2.pyrMeanShiftFiltering(image, 21, 51)
 # cv2.imshow("Input", image)
 
 # convert the mean shift image to grayscale, then apply
 # Otsu's thresholding
 gray = cv2.cvtColor(shifted, cv2.COLOR_BGR2GRAY)
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+thresh = cv2.threshold(gray, 0, 255,
+	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 # cv2.imshow("Thresh", thresh)
 
-# find contours in the thresholded image
-cnts = cv2.findContours(thresh.copy(),
-                        cv2.RETR_EXTERNAL,
-                        cv2.CHAIN_APPROX_SIMPLE)[-2]
-print("[INFO] {} unique contours found".format(len(cnts)))
+# compute the exact Euclidean distance from every binary
+# pixel to the nearest zero pixel, then find peaks in this
+# distance map
+D = ndimage.distance_transform_edt(thresh)
+localMax = peak_local_max(D, indices=False, min_distance=20,
+	labels=thresh)
 
-# loop over the contours
-for (i, c) in enumerate(cnts):
-    # draw the contour
-    ((x, y), _) = cv2.minEnclosingCircle(c)
-    cv2.putText(image,
-                "#{}".format(i + 1), (int(x) - 10, int(y)),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+# perform a connected component analysis on the local peaks,
+# using 8-connectivity, then appy the Watershed algorithm
+markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
+labels = watershed(-D, markers, mask=thresh)
+print("[INFO] {} unique segments found".format(len(np.unique(labels)) - 1))
+
+# loop over the unique labels returned by the Watershed
+# algorithm
+for label in np.unique(labels):
+	# if the label is zero, we are examining the 'background'
+	# so simply ignore it
+	if label == 0:
+		continue
+
+	# otherwise, allocate memory for the label region and draw
+	# it on the mask
+	mask = np.zeros(gray.shape, dtype="uint8")
+	mask[labels == label] = 255
+
+	# detect contours in the mask and grab the largest one
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	c = max(cnts, key=cv2.contourArea)
+
+	# draw a circle enclosing the object
+	((x, y), r) = cv2.minEnclosingCircle(c)
+	cv2.circle(image, (int(x), int(y)), int(r), (0, 255, 0), 2)
+	cv2.putText(image, "#{}".format(label), (int(x) - 10, int(y)),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
 # show the output image
-cv2.imshow("Image", image)
+cv2.imshow("Output", image)
 cv2.waitKey(0)
