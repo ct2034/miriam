9ae05106316b884c9c5dba5800918a68252eb79a
Christian Henkel <post@henkelchristian.de>
2019-03-01 11:38:50 +0100

    viewing plots from pkl files

---
+++ roadmaps/adamsmap/view.py
@@ -0,0 +1,17 @@
+#!python3
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import sys
+
+if __name__ == '__main__':
+    with open(sys.argv[1], "rb") as f:
+        store = pickle.load(f)
+
+    f, ax = plt.subplots()
+    legends = []
+    for k, v in store.items():
+        ax.plot(v)
+        legends.append(k)
+    plt.legend(legends)
+    plt.show()
