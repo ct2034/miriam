d487d4a3e358b670dc1eed280212f5d277b6b9ac
Christian Henkel <post@henkelchristian.de>
2019-03-02 07:03:36 +0100

    confergence pic

--- roadmaps/adamsmap/view.py
+++ roadmaps/adamsmap/view.py
@@ -9,11 +9,11 @@ plt.rcParams["font.family"] = "serif"
 plt.rcParams["savefig.dpi"] = 500
 
 if __name__ == '__main__':
-    res = np.zeros([3, 2048])
+    res = np.zeros([3, 4096])
     i = 0
-    for fname in ["x_200_2048.pkl",
-                  "x_500_2048.pkl",
-                  "x_1000_2048.pkl"]:
+    for fname in ["z_200_4096.pkl",
+                  "z_500_4096.pkl",
+                  "z_1000_4096.pkl"]:
         with open(fname, "rb") as f:
             store = pickle.load(f)
         res[i, :] = store['batchcost']
