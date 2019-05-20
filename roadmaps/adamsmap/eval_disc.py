df324691f8fb1618935dfd41539ba4ea07655896
Christian Henkel <post@henkelchristian.de>
2019-05-20 15:36:18 +0200

    is this working?!

--- roadmaps/adamsmap/eval_disc.py
+++ roadmaps/adamsmap/eval_disc.py
@@ -25,7 +25,7 @@ from adamsmap_filename_verification import (
 )
 
 
-def eval_disc(batch, nn, g, posar, edgew):
+def eval_disc(batch, nn, g, posar, edgew, agent_size):
     """
     Evaluating a given graph by simulating disc-shaped robots to travel through it. The robots move to their individual
         goals and whenever they would collide, they stop based on an arbitrary priority.
@@ -35,6 +35,7 @@ def eval_disc(batch, nn, g, posar, edgew):
     :param g: the graph to plan on (undirected)
     :param posar: poses of the graph nodes
     :param edgew: edge weights of the agents (determining the direction of the edges)
+    :param agent_size: how big is the agents disc
     :return: sum of costs, paths
     """
     sim_paths = []
@@ -46,44 +47,62 @@ def eval_disc(batch, nn, g, posar, edgew):
             coord_p[1:(1+len(p)), :] = np.array([posar[i_p] for i_p in p])
             coord_p[(1+len(p)), :] = batch[i_b, 1]
             goal = batch[i_b, 1]
-            sim_path = move_to_next_point(goal, coord_p)
+            sim_path = simulate_one_path(goal, coord_p)
             sim_paths.append(np.array(sim_path))
         else:
             print("Path failed !!")
             sim_paths.append(np.array([batch[i_b, 0]]))
-    sim_paths_coll = None
+    t_end, sim_paths_coll = simulate_paths_and_waiting(sim_paths, agent_size)
+    return sum(t_end), sim_paths_coll
 
+
+def simulate_paths_and_waiting(sim_paths, agent_size):
+    """
+    Simulate paths over time and let robots stop if required.
+
+    :param sim_paths: the coordinate based paths
+    :param agent_size: how big is the agents disc
+    :return: times when agents finished, actual paths
+    """
+    sim_paths_coll = None
     ended = [False for _ in range(agents)]
     waiting = [False for _ in range(agents)]
     i_per_agent = [0 for _ in range(agents)]
     t_end = [0 for _ in range(agents)]
-    time_slice = np.zeros([agents, 2])
     while not all(ended):
-        ended = [sim_paths[i].shape[0] - 1 == i_per_agent[i]
-                 for i in range(agents)]
-        for i_a in range(agents):
-            time_slice[i_a, :] = sim_paths[i_a][i_per_agent[i_a]]
-            if ended[i_a]:
-                t_end[i_a] = i_per_agent[i_a]
-        if sim_paths_coll is None:
-            sim_paths_coll = np.array([time_slice, ])
-        else:
-            sim_paths_coll = np.append(sim_paths_coll,
-                                       np.array([time_slice, ]),
-                                       axis=0)
-        for (a, b) in combinations(range(agents), r=2):
-            waiting = [False for _ in range(agents)]
-            if dist(sim_paths[a][i_per_agent[a]],
-                    sim_paths[b][i_per_agent[b]]) < agent_d:
-                waiting[min(a, b)] = True
-        i_per_agent = [i_per_agent[i] + (1 if not waiting[i]
-                                         and not ended[i]
-                                         else 0)
-                       for i in range(agents)]
-    return sum(t_end), sim_paths_coll
+        sim_paths_coll, ended, t_end, waiting, i_per_agent = iterate_sim(
+            t_end, waiting, i_per_agent, sim_paths, sim_paths_coll, agent_size
+        )
+    return t_end, sim_paths_coll
 
 
-def move_to_next_point(goal, coord_p):
+def iterate_sim(t_end, waiting, i_per_agent, sim_paths, sim_paths_coll, agent_size):
+    ended = [sim_paths[i].shape[0] - 1 == i_per_agent[i]
+             for i in range(agents)]
+    time_slice = np.zeros([agents, 2])
+    for i_a in range(agents):
+        time_slice[i_a, :] = sim_paths[i_a][i_per_agent[i_a]]
+        if ended[i_a]:
+            t_end[i_a] = i_per_agent[i_a]
+    if sim_paths_coll is None:
+        sim_paths_coll = np.array([time_slice, ])
+    else:
+        sim_paths_coll = np.append(sim_paths_coll,
+                                   np.array([time_slice, ]),
+                                   axis=0)
+    for (a, b) in combinations(range(agents), r=2):
+        waiting = [False for _ in range(agents)]
+        if dist(sim_paths[a][i_per_agent[a]],
+                sim_paths[b][i_per_agent[b]]) < agent_size:
+            waiting[min(a, b)] = True
+    i_per_agent = [i_per_agent[i] + (1 if not waiting[i]
+                                     and not ended[i]
+                                     else 0)
+                   for i in range(agents)]
+    return sim_paths_coll, ended, t_end, waiting, i_per_agent
+
+
+def simulate_one_path(goal, coord_p):
     """
     Simulate one agent path through coordinates.
 
@@ -128,10 +147,9 @@ if __name__ == '__main__':
         res[ans]["paths_undirected"] = []
         res[ans]["paths_random"] = []
 
-    for agents, agent_d, i in product(
-        [20], [20], range(2)):
+    for agents, agent_size, _ in product(
+        agent_ns, [40], range(2)):
         print("agents: " + str(agents))
-        print("agent_d: " + str(agent_d))
         v = 1
         nn = 1
         posar = store['posar']
@@ -142,16 +160,16 @@ if __name__ == '__main__':
         make_edges(N, g, ge, posar, edgew, im)
         batch = np.array([
             [get_random_pos(im), get_random_pos(im)] for _ in range(agents)])
-        cost_ev, paths_ev = eval_disc(batch, nn, ge, posar, edgew)
+        cost_ev, paths_ev = eval_disc(batch, nn, ge, posar, edgew, agent_size)
 
         edgew_undirected = np.ones([N, N])
         g_undirected = nx.Graph()
         g_undirected.add_nodes_from(range(N))
         for e in nx.edges(ge):
             g_undirected.add_edge(e[0],
-                               e[1],
-                               distance=dist(posar[e[0]], posar[e[1]]))
-        cost_undirected, paths_undirected = (eval_disc(batch, nn, g_undirected, posar, edgew_undirected))
+                                  e[1],
+                                  distance=dist(posar[e[0]], posar[e[1]]))
+        cost_undirected, paths_undirected = (eval_disc(batch, nn, g_undirected, posar, edgew_undirected, agent_size))
 
         g_random = nx.Graph()
         g_random.add_nodes_from(range(N))
@@ -165,23 +183,23 @@ if __name__ == '__main__':
             fakenodes1, fakenodes2, axis=0), axis=0
         ))
         (indptr, indices) = tri.vertex_neighbor_vertices
-        for i in range(N):
-            neigbours = indices[indptr[i]:indptr[i+1]]
+        for i_n in range(N):
+            neigbours = indices[indptr[i_n]:indptr[i_n + 1]]
             for n in neigbours:
-                if i < n & n < N:
+                if (i_n < n) & (n < N):
                     line = bresenham(
-                        int(posar_random[i][0]),
-                        int(posar_random[i][1]),
+                        int(posar_random[i_n][0]),
+                        int(posar_random[i_n][1]),
                         int(posar_random[n][0]),
                         int(posar_random[n][1])
                     )
                     # print(list(line))
                     if all([is_pixel_free(im, x) for x in line]):
-                        g_random.add_edge(i, n,
-                                   distance=dist(posar_random[i], posar_random[n]))
-                        g_random.add_edge(n, i,
-                                   distance=dist(posar_random[i], posar_random[n]))
-        cost_random, paths_random = eval_disc(batch, nn, g_random, posar_random, edgew_undirected)
+                        g_random.add_edge(i_n, n,
+                                          distance=dist(posar_random[i_n], posar_random[n]))
+                        g_random.add_edge(n, i_n,
+                                          distance=dist(posar_random[i_n], posar_random[n]))
+        cost_random, paths_random = eval_disc(batch, nn, g_random, posar_random, edgew_undirected, agent_size)
 
         print("our: %d, undir: %d, (our-undir)/our: %.3f%%" %
               (cost_ev, cost_undirected,
