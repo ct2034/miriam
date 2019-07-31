6da0b3e7f6e0fbe6dc6ca567ac419c0478646876
Christian Henkel <post@henkelchristian.de>
2019-07-31 23:16:29 +0200

    simulating paths in gazebo

--- roadmaps/adamsmap_gazebo/src/publish_robot_pose.py
+++ roadmaps/adamsmap_gazebo/src/publish_robot_pose.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python2
+import csv
 import numpy as np
 import random
 import rospy
+import sys
 
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SpawnModel, SpawnModelRequest
 
 SET_MODEL_TOPIC_NAME = '/gazebo/set_model_state'
 SPAWN_SERVICE_NAME = '/gazebo/spawn_urdf_model'
-SIZE = 45
-D = .05
+SCALE = .1
+SHIFT = -48
 
 
 def publish_pose(pub, name, pos):
@@ -17,34 +19,34 @@ def publish_pose(pub, name, pos):
     ms = ModelState()
     ms.model_name = name
     ms.pose.position.x = x
-    ms.pose.position.y = y
-    ms.pose.position.z = -1
+    ms.pose.position.y = -y
+    ms.pose.position.z = -.5
     pub.publish(ms)
 
 
 if __name__ == '__main__':
     initialized = False
-    n_agents = 10
     model_str = rospy.get_param('/robot_description')
     rospy.init_node('publish_robot_pose')
     pub = rospy.Publisher(SET_MODEL_TOPIC_NAME, ModelState, queue_size=1)
     rospy.wait_for_service(SPAWN_SERVICE_NAME)
     client = rospy.ServiceProxy(SPAWN_SERVICE_NAME, SpawnModel)
-    poses = np.zeros([n_agents, 2])
-    while(True):
-        for i in range(n_agents):
-            name = "robot{}".format(i, "%02d")
+    fname = sys.argv[1]
+    with open(fname, 'r') as f :
+        reader = csv.reader(f, delimiter=' ')
+        for row in reader:
             if not initialized:
-                poses[i, :] = [2 * SIZE * random.random() - SIZE,
-                               2 * SIZE * random.random() - SIZE]
-                req = SpawnModelRequest()
-                req.model_name = name
-                req.model_xml = model_str
-                res = client(req)
-                print(res)
-            poses[i, :] = (np.array([D * random.random() - D / 2,
-                                     D * random.random() - D / 2])
-                           + poses[i, :])
-            publish_pose(pub, name, poses[i, :])
-        initialized = True
-        rospy.sleep(.01)
+                n_agents = len(row) / 2
+            for i_a in range(n_agents):
+                name = "robot{}".format(i_a, "%02d")
+                pose = [SCALE * float(row[i_a*2]) + SHIFT,
+                        SCALE * float(row[i_a*2+1]) + SHIFT]
+                if not initialized:
+                    req = SpawnModelRequest()
+                    req.model_name = name
+                    req.model_xml = model_str
+                    res = client(req)
+                    print(res)
+                publish_pose(pub, name, pose)
+            initialized = True
+            rospy.sleep(.01)
