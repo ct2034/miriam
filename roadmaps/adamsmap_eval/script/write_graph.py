d5ebe5a84b5649af7bed76602f20e41938b13ab9
Christian Henkel <post@henkelchristian.de>
2019-09-13 15:43:10 +0200

    adding a csv for undirected graph

--- roadmaps/adamsmap_eval/script/write_graph.py
+++ roadmaps/adamsmap_eval/script/write_graph.py
@@ -2,6 +2,7 @@
 from adamsmap.adamsmap import graphs_from_posar, make_edges, plot_graph
 from adamsmap_eval.filename_verification import (
     get_graph_csvs,
+    get_graph_undir_csv,
     is_result_file,
     resolve_mapname,
     get_basename_wo_extension
@@ -30,19 +31,24 @@ if __name__ == '__main__':
     edgew = res['edgew']
     map_im = imageio.imread(resolve_mapname(fname))
     N = posar.shape[0]
-    g, _, pos = graphs_from_posar(posar.shape[0], posar)
+    _, g, pos = graphs_from_posar(posar.shape[0], posar)
+    make_edges(N, _, g, posar, edgew, map_im)
     if sys.argv[1] == "fig":
         graph_fig = plt.figure(figsize=figsize)
         ax = graph_fig.add_subplot(111)
-        make_edges(N, g, _, posar, edgew, map_im)
         plt.gca().set_position([0, 0, 1, 1])
         plot_graph(graph_fig, ax, g, pos, edgew, map_im,
                    fname="res/" + basename + ".final_sq.png")
         plt.show()
     elif sys.argv[1] == "csv":
         fname_adjlist, fname_pos = get_graph_csvs(fname)
-        nx.write_adjlist(g, fname_adjlist)                   #
-        with open(fname_pos, 'w') as f_csv:                       #
+        fname_adjlist_undir = get_graph_undir_csv(fname)
+        nx.write_adjlist(g, fname_adjlist)
+        g_undir = g.copy()
+        for e in g.edges:
+            g_undir.add_edge(e[1], e[0])
+        nx.write_adjlist(g_undir, fname_adjlist_undir)
+        with open(fname_pos, 'w') as f_csv:
             writer = csv.writer(f_csv, delimiter=' ')
             for i_a in range(N):
                 writer.writerow(posar[i_a])
--- roadmaps/adamsmap_eval/src/adamsmap_eval/filename_verification.py
+++ roadmaps/adamsmap_eval/src/adamsmap_eval/filename_verification.py
@@ -4,6 +4,7 @@ import re
 BASE_NAME = ".*\/[a-z]_[0-9]+_[0-9]+\."
 RESULT_FILE = re.compile(BASE_NAME + "pkl$")
 EVAL_FILE = re.compile(BASE_NAME + "pkl.eval$")
+EVAL_CEN_FILE = re.compile(BASE_NAME + "eval_cen.pkl$")
 
 
 def is_result_file(fname):
@@ -12,6 +13,10 @@ def is_result_file(fname):
 
 def is_eval_file(fname):
     return bool(EVAL_FILE.match(fname))
+
+
+def is_eval_cen_file(fname):
+    return bool(EVAL_CEN_FILE.match(fname))
 
 
 def get_basename_wo_extension(fname):
