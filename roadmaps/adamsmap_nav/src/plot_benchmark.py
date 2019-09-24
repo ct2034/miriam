7c1d63d9b8e3094f7165e8899dbe8b24c07c2a91
Christian Henkel <post@henkelchristian.de>
2019-09-24 10:39:23 +0200

    finished figure

--- roadmaps/adamsmap_nav/src/plot_benchmark.py
+++ roadmaps/adamsmap_nav/src/plot_benchmark.py
@@ -4,6 +4,8 @@ import os
 import pickle
 import sys
 
+ODRM_STR = "ODRM-based"
+
 if __name__ == "__main__":
     plt.style.use('bmh')
     plt.rcParams["font.family"] = "serif"
@@ -25,19 +27,33 @@ if __name__ == "__main__":
                 ns_agents.append(fname.split(".")[1].split("-")[-1])
                 planner = fname.split("-")[0]
                 if planner == "adamsmap_global_planner":
-                    planners.append("ODRM-based")
+                    planners.append(ODRM_STR)
                 else:
                     planners.append("Gridmap-based")
 
 
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 4))
     violin_parts = ax.violinplot(data, showmeans=True, showextrema=False)
-    violin_parts['cmeans'].set_color("C1")
     idx = range(len(data))
+    violin_parts['cmeans'].set_color("C1")
+    for i in idx:
+        if planners[i] == ODRM_STR:
+            violin_parts['bodies'][i].set_facecolor("C3")
+        else:
+            violin_parts['bodies'][i].set_facecolor("C2")
+    print(violin_parts['cmeans'])
     ax.set_xticks([x + 1 for x in idx])
+
+    def get_label(x):
+        if x == 1 or x == 4:
+            return planners[x] + "\n" + ns_agents[x] + " Agents"
+        else:
+            return "\n" + ns_agents[x] + " Agents"
+
     ax.set_xticklabels(
-        map(lambda x: planners[x] + "\n" + ns_agents[x] + " Agents", idx)
+        map(get_label, idx)
     )
-    ax.set_ylabel("Max Runtime [s]")
+    ax.set_ylabel("Max Travel Duration [s]")
+    # plt.title("Decentralized Planner Travel Duration")
     plt.tight_layout()
     fig.savefig(folder + 'decentralized_travel_times.png')
