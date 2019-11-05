5ad0be4f5b6e433c1a5ccdcb4656bf59d3e16848
Christian Henkel <post@henkelchristian.de>
2019-11-05 12:32:03 +0800

    configurable kernel size

--- learn/tf/gaussian.py
+++ learn/tf/gaussian.py
@@ -1,68 +1,52 @@
 import tensorflow as tf
-from PIL import Image    
+from PIL import Image
 import numpy as np
 import math
+from itertools import product
 
+
+def getGaussValue(kerStd, posX, posY):
+    return 1./(2.*math.pi*(np.power(kerStd, 2)))*math.exp(-(np.power(posX, 2)+np.power(posY, 2))/(2.*(np.power(kerStd, 2))))
+
+
+def getGaussKernel(kerStd, kerSize, datSize):
+    assert kerSize % 2 == 1, "use with odd kernel size"
+    d = (kerSize-1)/2
+    d_idxs = range(int(-d), int(d+1), 1)
+    kernel = np.zeros([kerSize, kerSize, datSize, datSize])
+    
+    for ix, iy in product(range(kerSize), repeat=2):
+        dx = d_idxs[ix]
+        dy = list(reversed(d_idxs))[iy]
+        kernel[ix, iy] = np.eye(datSize) * getGaussValue(kerStd, dx, dy)
+    
+    return tf.constant(kernel, dtype=tf.float32)
+
+
+def getImageData(fileNameList):
+    imageData = []
+    for fn in fileNameList:
+        testImage = Image.open(fn)
+        testImage.show()
+        imageData.append(np.array(testImage))
+    return np.array(imageData, dtype=np.float32)
+
+
+def blur(g, y):
+    with tf.Session(graph=g) as sess:
+        return sess.run(y)[0]
 
 
 g = tf.Graph()
-
 with g.as_default():
-    
-    def getGuessValue(kerStd,posX,posY):
-        return 1./(2.*math.pi*(np.power(kerStd,2)))*math.exp(-(np.power(posX,2)+np.power(posY,2))/(2.*(np.power(kerStd,2))))
-    
-    def getGaussKernel(kerStd):
-        K11=np.column_stack((np.row_stack((np.eye(3)*getGuessValue(kerStd,-1,1),[0.,0.,0.])),np.array([0.,0.,0.,1.])))
-        K12=np.column_stack((np.row_stack((np.eye(3)*getGuessValue(kerStd,0,1),[0.,0.,0.])),np.array([0.,0.,0.,1.])))        
-        K13=np.column_stack((np.row_stack((np.eye(3)*getGuessValue(kerStd,1,1),[0.,0.,0.])),np.array([0.,0.,0.,1.])))      
-        K21=np.column_stack((np.row_stack((np.eye(3)*getGuessValue(kerStd,-1,0),[0.,0.,0.])),np.array([0.,0.,0.,1.])))      
-        K22=np.column_stack((np.row_stack((np.eye(3)*getGuessValue(kerStd,0,0),[0.,0.,0.])),np.array([0.,0.,0.,1.])))      
-        K23=np.column_stack((np.row_stack((np.eye(3)*getGuessValue(kerStd,1,0),[0.,0.,0.])),np.array([0.,0.,0.,1.])))      
-        K31=np.column_stack((np.row_stack((np.eye(3)*getGuessValue(kerStd,-1,-1),[0.,0.,0.])),np.array([0.,0.,0.,1.])))      
-        K32=np.column_stack((np.row_stack((np.eye(3)*getGuessValue(kerStd,0,-1),[0.,0.,0.])),np.array([0.,0.,0.,1.])))      
-        K33=np.column_stack((np.row_stack((np.eye(3)*getGuessValue(kerStd,1,-1),[0.,0.,0.])),np.array([0.,0.,0.,1.])))      
-        print(K11.shape)
-        kernel=tf.constant(np.array(
-                [
-                    [
-                       K11,
-                       K12,
-                       K13
-                    ],
-                    [
-                       K21,
-                       K22,
-                       K23                    
-                    ],
-                    [
-                       K31,
-                       K32,
-                       K33                    
-                    ]              
-                ])
-                ,dtype=tf.float32)#3*3*4*4
-        return kernel
-
-    def getImageData(fileNameList):
-        imageData=[]
-        for fn in fileNameList:        
-            testImage = Image.open(fn)
-            testImage.show() 
-            imageData.append(np.array(testImage))
-        return np.array(imageData,dtype=np.float32)
-
-    imageData=getImageData(("dog.png",))
-    testData=tf.constant(imageData)
-    kernel=getGaussKernel(0.8)
-    y=tf.cast(tf.nn.conv2d(testData,kernel,strides=[1,1,1,1],padding="SAME"), dtype=tf.int32)
+    imageData = getImageData(("tf/dog.png",))
+    testData = tf.constant(imageData)
+    kernel = getGaussKernel(2.0, 11, 3)
+    y = tf.cast(tf.nn.conv2d(testData, kernel, strides=[
+                1, 1, 1, 1], padding="VALID"), dtype=tf.int32)
     init_op = tf.global_variables_initializer()
-with tf.Session(graph=g) as sess:
-    print(testData.get_shape())
-    # print(kernel.eval())
-    # print(kernel.get_shape())
-    resultData=sess.run(y)[0]
-    # print(resultData.shape)
-    resulImage=Image.fromarray(np.uint8(resultData))   
-    resulImage.show()
-    # print(y.get_shape())
+
+print(testData.get_shape())
+resultData = blur(g, y)
+resulImage = Image.fromarray(np.uint8(resultData))
+resulImage.show()
