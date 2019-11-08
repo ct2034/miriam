be36f08da6703d5c4e1a39582baf93c91b558c20
Christian Henkel <post@henkelchristian.de>
2019-11-08 11:33:45 +0800

    trying to learn sin

--- learn/tf/lstm.py
+++ learn/tf/lstm.py
@@ -12,44 +12,53 @@ from __future__ import print_function
 
 import tensorflow as tf
 from tensorflow.contrib import rnn
-
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-
-'''
-To classify images using a recurrent neural network, we consider every image
-row as a sequence of pixels. Because MNIST image shape is 28*28px, we will then
-handle 28 sequences of 28 steps for every sample.
-'''
+import numpy as np
+import random
 
 # Training Parameters
 learning_rate = 0.001
-training_steps = 10000
+training_steps = 1000
 batch_size = 128
-display_step = 200
+display_step = 10
 
 # Network Parameters
-num_input = 28 # MNIST data input (img shape: 28*28)
-timesteps = 28 # timesteps
+num_inputs = 1 # one y value per timestep
+timesteps = 1000 # timesteps
 num_hidden = 128 # hidden layer num of features
-num_classes = 10 # MNIST total classes (0-9 digits)
+num_outputs = 1 # function value
+
+t = np.linspace(0., 2*np.pi, timesteps-1)[:-1]
+s = np.sin(t) + np.sin(2*t - 3/2) + np.sin(3*t - 4)
+
+def get_sample():
+    d = random.randint(0, timesteps-1)
+    x = s[d:] + s[:d]
+    y = s[d]
+    return tf.constant(x), tf.constant(y)
+
+def get_batch(n):
+    xs = []
+    ys = []
+    for _ in range(n):
+        x, y = get_sample()
+        xs.append(x)
+        ys.append(y)
+    return tf.constant(xs), tf.constant(ys) 
 
 # tf Graph input
-X = tf.placeholder("float", [None, timesteps, num_input])
-Y = tf.placeholder("float", [None, num_classes])
+X = tf.placeholder("float", [None, timesteps, num_inputs])
+Y = tf.placeholder("float", [None, num_outputs])
 
 # Define weights
 weights = {
-    'out': tf.Variable(tf.random_normal([num_hidden, num_classes]))
+    'out': tf.Variable(tf.random_normal([num_hidden, num_outputs]))
 }
 biases = {
-    'out': tf.Variable(tf.random_normal([num_classes]))
+    'out': tf.Variable(tf.random_normal([num_outputs]))
 }
 
 
 def RNN(x, weights, biases):
-
     # Prepare data shape to match `rnn` function requirements
     # Current data input shape: (batch_size, timesteps, n_input)
     # Required shape: 'timesteps' tensors list of shape (batch_size, n_input)
@@ -66,12 +75,10 @@ def RNN(x, weights, biases):
     # Linear activation, using rnn inner loop last output
     return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
-logits = RNN(X, weights, biases)
-prediction = tf.nn.softmax(logits)
+prediction = RNN(X, weights, biases)
 
 # Define loss and optimizer
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-    logits=logits, labels=Y))
+loss_op = tf.pow(tf.subtract(Y, prediction), 2)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss_op)
 
@@ -89,9 +96,8 @@ with tf.Session() as sess:
     sess.run(init)
 
     for step in range(1, training_steps+1):
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
-        # Reshape data to get 28 seq of 28 elements
-        batch_x = batch_x.reshape((batch_size, timesteps, num_input))
+        batch_x, batch_y = get_batch(batch_size)
+
         # Run optimization op (backprop)
         sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
         if step % display_step == 0 or step == 1:
@@ -106,7 +112,7 @@ with tf.Session() as sess:
 
     # Calculate accuracy for 128 mnist test images
     test_len = 128
-    test_data = mnist.test.images[:test_len].reshape((-1, timesteps, num_input))
+    test_data = mnist.test.images[:test_len].reshape((-1, timesteps, num_inputs))
     test_label = mnist.test.labels[:test_len]
     print("Testing Accuracy:", \
         sess.run(accuracy, feed_dict={X: test_data, Y: test_label}))
