1e20b1836f0ed4bdc9b1fd54fdb8064f1e0e8b21
Christian Henkel <post@henkelchristian.de>
2019-12-17 15:22:14 +0100

    looking at the data

--- planner/policylearn/generate_data.py
+++ planner/policylearn/generate_data.py
@@ -6,7 +6,11 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-from plan_ecbs import plan_in_gridmap
+from plan_ecbs import plan_in_gridmap, BLOCKS_STR
+
+
+VERTEX_CONSTRAINTS_STR = 'vertexConstraints'
+SCHEDULE_STR = 'schedule' 
 
 
 def make_random_gridmap(width, height, fill) -> np.ndarray:
@@ -26,7 +30,6 @@ def show_map(x):
         aspect='equal',
         cmap=plt.get_cmap("binary"),
         origin='lower')
-    plt.show()
 
 
 def is_free(gridmap, pos):
@@ -45,25 +48,73 @@ def get_random_free_pos(gridmap, width, height):
     return pos
 
 
+def get_vertex_block_coords(blocks):  # TODO: edge constraints
+    coords = []
+    for agent in blocks.keys():
+        coords_pa = []
+        if blocks[agent]:
+            blocks_pa = blocks[agent]
+            if VERTEX_CONSTRAINTS_STR in blocks_pa.keys():
+                for vc in blocks_pa[VERTEX_CONSTRAINTS_STR]:
+                    coords_pa.append([vc['v.x'], vc['v.y']])
+        coords.append(np.array(coords_pa))
+    return coords
+
+
+def get_agent_paths_from_data(data):
+    agent_paths = []
+    if SCHEDULE_STR in data.keys():
+        schedule = data[SCHEDULE_STR]
+        for agent_str in schedule.keys():
+            path_pa = []
+            schedule_pa = schedule[agent_str]
+            for pose in schedule_pa:
+                path_pa.append([
+                    pose['x'],
+                    pose['y']
+                ])
+            agent_paths.append(np.array(path_pa))
+    return agent_paths
+
+
 if __name__ == "__main__":
     logging.basicConfig()
-    logger = logging.getLogger('benchmark_ecbs')
-    logger.setLevel(logging.WARN)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
 
+    plot = True
     width = 8
     height = 8
     random.seed(1)
     n_agents = 3
     gridmap = make_random_gridmap(width, height, .4)
     count_blocks = 0
-    while count_blocks < 10:
+    colors = ['r', 'b', 'g', 'c', 'm', 'y', 'k']
+    while count_blocks < 2:
         starts = [get_random_free_pos(gridmap, width, height)
                   for _ in range(n_agents)]
         goals = [get_random_free_pos(gridmap, width, height)
                  for _ in range(n_agents)]
-        blocks = plan_in_gridmap(gridmap, starts, goals)
-        if blocks:
+        data = plan_in_gridmap(gridmap, starts, goals)
+
+        if data and BLOCKS_STR in data.keys():
+            blocks = data[BLOCKS_STR]
             has_blocks = not all(v == 0 for v in blocks.values())
             if has_blocks:
                 count_blocks += 1
-                print("blocks:" + str(blocks))
+                logger.info("blocks:" + str(blocks))
+                if plot:
+                    show_map(gridmap)
+                    block_coords = get_vertex_block_coords(blocks)
+                    agent_paths = get_agent_paths_from_data(data)
+                    for ia in range(len(agent_paths)):
+                        plt.plot(agent_paths[ia][:, 0],
+                                 agent_paths[ia][:, 1],
+                                 '-',
+                                 color=colors[ia])
+                        if block_coords[ia].shape[0]:
+                            plt.plot(block_coords[ia][:, 0],
+                                    block_coords[ia][:, 1],
+                                    'x',
+                                    color=colors[ia])
+                    plt.show()
