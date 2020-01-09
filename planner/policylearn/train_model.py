a91b8008aaf187248fe06fb54baa9167a0eebaa1
Christian Henkel <post@henkelchristian.de>
2020-01-09 13:54:41 +0100

    train in batches

--- planner/policylearn/train_model.py
+++ planner/policylearn/train_model.py
@@ -12,15 +12,28 @@ import numpy as np
 from generate_data import OTHERS_STR, OWN_STR
 
 
-def get_training_step(training_data, step):
-    X, y = training_data[step]
-    x = []
-    for i_t in range(len(X[OWN_STR])):
-        xt = X[OWN_STR][i_t]
-        for i_a in range(len(X[OTHERS_STR])):
-            xt = np.append(xt, X[OTHERS_STR][i_a][i_t])
-        x.append(xt)
-    return x, [y]  # TODO: bigger batches
+def get_training_steps(training_data, start_step, batch_size, num_input, num_timesteps):
+    xy_batch = training_data[start_step:(start_step+batch_size)]
+    X_out = []
+    Y_out = []
+    timesteps = []
+    for i_b in range(batch_size):
+        timesteps.append(len(xy_batch[i_b][0][OWN_STR]))
+    assert num_timesteps > max(timesteps),  "must have at least the timesteps"
+    for i_b in range(batch_size):
+        x = []
+        this_timesteps = len(xy_batch[i_b][0][OWN_STR])
+        pad = num_timesteps - this_timesteps  # padding, tipp by https://github.com/keras-team/keras/issues/85#issuecomment-96425996
+        for i_t in range(pad):
+            x.append(np.zeros(num_input))
+        for i_t in range(this_timesteps):
+            xt = xy_batch[i_b][0][OWN_STR][i_t]
+            for i_a in range(len(xy_batch[i_b][0][OTHERS_STR])):
+                xt = np.append(xt, xy_batch[i_b][0][OTHERS_STR][i_a][i_t])
+            x.append(xt)
+        X_out.append(x)
+        Y_out.append([xy_batch[i_b][1],])
+    return X_out, Y_out
 
 
 if __name__ == "__main__":
@@ -44,10 +57,11 @@ if __name__ == "__main__":
     # --------------------------------------------------------------------------------
     # model
     learning_rate = 0.01
-    training_steps = int(len(training_data) * .8)
+    training_precentage = .9
+    training_steps = int(len(training_data) * training_precentage)
     validation_steps = len(training_data) - training_steps
-    # batch_size = 10
-    display_step = 100
+    batch_size = 10
+    display_step = 10
 
     num_inputs_other = training_data[0][0][OTHERS_STR][0][0].shape[0]
     num_others = len(training_data[0][0][OTHERS_STR])
@@ -56,11 +70,10 @@ if __name__ == "__main__":
     num_com_channels = 3  # how many colors has the image  TODO: use blurmap
     num_hidden = 64  # hidden layers other and self
     num_classes = 1  # one class for 0. .. 1.
+    num_timesteps = 20  # TODO: get from generated data
 
-    timesteps = None  # to be defined at runtime ...
-
-    X = tf.placeholder("float", [timesteps, num_input], name="X")
-    Y = tf.placeholder("float", [num_classes], name="Y")
+    X = tf.placeholder("float", [batch_size, num_timesteps, num_input], name="X")
+    Y = tf.placeholder("float", [batch_size, num_classes], name="Y")
 
     weights = {
         'out': tf.Variable(tf.random_normal([num_hidden, num_classes]), name="weights")
@@ -69,12 +82,14 @@ if __name__ == "__main__":
         'out': tf.Variable(tf.random_normal([num_classes]), name="biases")
     }
 
-    def LSTM(x, weights, biases):    
+    def LSTM(x, weights, biases):
+        x = tf.unstack(x, None, 1)
+
         # Define a lstm cell with tensorflow
         lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
 
         # Get lstm cell output
-        outputs, states = rnn.static_rnn(lstm_cell, [x], dtype=tf.float32)
+        outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
 
         # Linear activation, using rnn inner loop last output
         return tf.matmul(outputs[-1], weights['out']) + biases['out']
@@ -99,15 +114,19 @@ if __name__ == "__main__":
         # Run the initializer
         sess.run(init)
 
-        for step in range(training_steps):
-            batch_x, batch_y = get_training_step(training_data, step)
+        assert 0 == training_steps % batch_size, "training steps must be divisible by batch size"
+        for batch_step in range(int(training_steps / batch_size)):
+            start_step = batch_step * batch_size
+            batch_x, batch_y = get_training_steps(
+                training_data, start_step, batch_size, num_input, num_timesteps)
             # Run optimization op (backprop)
-            sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
-            if step % display_step == 0 or step == 1:
+            sess.run(train_op, feed_dict={X: batch_x,
+                                          Y: batch_y})
+            if batch_step % display_step == 0 or batch_step == 1:
                 # Calculate batch loss and accuracy
                 loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
                                                                      Y: batch_y})
-                print("Step " + str(step) + ", Minibatch Loss= " +
+                print("Step " + str(batch_step) + ", Minibatch Loss= " +
                       "{:.4f}".format(loss) + ", Training Accuracy= " +
                       "{:.3f}".format(acc))
 
@@ -122,4 +141,3 @@ if __name__ == "__main__":
         # test_label = argmax_label(test_label)
         # print("Testing Accuracy:",
         #       sess.run(accuracy, feed_dict={X: test_data, Y: test_label}))
-
