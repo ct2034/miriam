00726b2cd7dd62da25bb825b5596ec5bbf5027b6
Christian Henkel <post@henkelchristian.de>
2020-04-10 14:01:10 +0200

    initializing agents

--- sim/decentralized/sim.py
+++ sim/decentralized/sim.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot as plt
 def initialize_environment(size, fill):
     """Make a square map with edge length `size` and
     `fill` (0..1) obstacle ratio."""
-    environent = np.zeros([size, size])
+    environent = np.zeros([size, size], dtype=np.int64)
     n_to_fill = int(fill * size ** 2)
     to_fill = random.sample(
         list(itertools.product(range(size), repeat=2)), k=n_to_fill)
@@ -20,7 +20,8 @@ def initialize_environment(size, fill):
 
 
 def plot(environent):
-    """Plot the environment map with `x` coordinates to the right, `y` up."""
+    """Plot the environment map with `x` coordinates to the right, `y` up. 
+    Occupied by colourful agents."""
     image = environent * -.5 + 1
     image = np.swapaxes(image, 0, 1)
     fig, ax = plt.subplots()
@@ -35,6 +36,30 @@ def plot(environent):
     plt.show()
 
 
+def initialize_new_agent(environent, agents):
+    """Place new agent in the environment, where no obstacle or other agent is."""
+    environent_with_agents = environent.copy()
+    for a in agents:
+        environent_with_agents[tuple(a)] = 1
+    no_obstacle = np.where(environent_with_agents == 0)
+    gen = np.random.default_rng()
+    return gen.choice(no_obstacle, axis=1)
+
+
+def initialize_agents(environent, n_agents):
+    agents = np.ndarray([0, 2], dtype=np.int64)
+    for i_a in range(n_agents):
+        agent = initialize_new_agent(environent, agents)
+        agents = np.append(agents, [agent], axis=0)
+    return agents
+
+
 if __name__ == "__main__":
+    # maze (environment)
     env = initialize_environment(10, .2)
+    
+    # agents
+    agents = initialize_agents(10)
+
+    # display
     plot(env)
--- sim/decentralized/sim_test.py
+++ sim/decentralized/sim_test.py
@@ -13,6 +13,22 @@ class TestDecentralizedSim(unittest.TestCase):
         self.assertEqual(env.shape, (10, 10))
         self.assertEqual(np.count_nonzero(env), 50)
 
+    def test_initialize_new_agent(self):
+        env_zz = np.array([[0, 1], [1, 1]])
+        zero_zero = sim.initialize_new_agent(env_zz, [])
+        self.assertTrue((zero_zero == [0, 0]).all())
+
+        env_zo = np.array([[0, 0], [1, 1]])
+        zero_one = sim.initialize_new_agent(env_zo, np.array([zero_zero]))
+        self.assertTrue((zero_one == [0, 1]).all())
+
+    def test_initialize_agents(self):
+        env = np.array([[0, 0], [0, 1]])
+        agents = sim.initialize_agents(env, 3)
+        self.assertEqual(len(agents), 3)
+        self.assertIn([0, 0], agents)
+        self.assertIn([0, 1], agents)
+        self.assertIn([1, 0], agents)
 
 if __name__ == "__main__":
     unittest.main()
