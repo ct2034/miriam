4e1d5f7f575d4557b784a569b582086872bd0e03
Christian Henkel <post@henkelchristian.de>
2020-08-03 10:38:35 +0200

    refactoring

--- scenarios/generators.py
+++ scenarios/generators.py
@@ -17,18 +17,22 @@ logging.getLogger('sim.decentralized.agent').setLevel(logging.ERROR)
 
 
 @cachier(hash_params=tools.hasher)
-def like_sim_decentralized(size: int, fill: float,
-                           n_agents: int, seed: Any):
-    random.seed(seed)
-    env = sim.decentralized.runner.initialize_environment(size, fill)
+def make_starts_goals_on_env(env: np.ndarray, n_agents: int):
     agents = sim.decentralized.runner.initialize_agents(
         env, n_agents, sim.decentralized.agent.Policy.RANDOM)
-
     starts = np.array([a.pos for a in agents])
     assert starts.shape == (n_agents, 2)
     goals = np.array([a.goal for a in agents])
     assert goals.shape == (n_agents, 2)
+    return starts, goals
 
+
+@cachier(hash_params=tools.hasher)
+def like_sim_decentralized(size: int, fill: float,
+                           n_agents: int, seed: Any):
+    random.seed(seed)
+    env = sim.decentralized.runner.initialize_environment(size, fill)
+    starts, goals = make_starts_goals_on_env(env, n_agents)
     return env, starts, goals
 
 
@@ -38,12 +42,5 @@ def like_policylearn_gen(size: int, fill: float,
     random.seed(seed)
     env = planner.policylearn.generate_data.generate_random_gridmap(
         size, size, fill)
-    agents = sim.decentralized.runner.initialize_agents(
-        env, n_agents, sim.decentralized.agent.Policy.RANDOM)
-
-    starts = np.array([a.pos for a in agents])
-    assert starts.shape == (n_agents, 2)
-    goals = np.array([a.goal for a in agents])
-    assert goals.shape == (n_agents, 2)
-
+    starts, goals = make_starts_goals_on_env(env, n_agents)
     return env, starts, goals
