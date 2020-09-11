eee1404fe893129593338dd0dc978013880bdf61
Christian Henkel <post@henkelchristian.de>
2020-09-11 08:06:15 +0200

    refactoring data modifications

--- planner/policylearn/train_model_classification.py
+++ planner/policylearn/train_model_classification.py
@@ -10,11 +10,13 @@ from matplotlib import pyplot as plt
 from tensorflow.keras.layers import (Conv2D, Conv3D, Dense, DepthwiseConv2D,
                                      Dropout, Flatten, MaxPooling2D, Reshape)
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+
+tf.compat.v1.GPUOptions(allow_growth=True)
 
 IMG_SIZE = 13
 IMG_DEPTH_T = 3
-IMG_DEPTH_FRAMES = 4
-TRAINING_PERC = 1
+IMG_DEPTH_FRAMES = 5
 
 
 if __name__ == "__main__":
@@ -30,69 +32,60 @@ if __name__ == "__main__":
     # data
     train_images_l = []
     train_labels_l = []
-    test_images = []
-    test_labels = []
-    for i in range(len(d)):
-        if i < TRAINING_PERC * len(d):  # training data
-            dat = d[i][0]
-            train_images_l.append(dat)
 
-            # more data
-            dat = np.rot90(dat)
-            train_images_l.append(dat)
-            dat = np.rot90(dat)
-            train_images_l.append(dat)
-            dat = np.rot90(dat)
-            train_images_l.append(dat)
-            # <->
-            dat = np.flip(dat, 0)
-            train_images_l.append(dat)
-            dat = np.rot90(dat)
-            train_images_l.append(dat)
-            dat = np.rot90(dat)
-            train_images_l.append(dat)
-            dat = np.rot90(dat)
-            train_images_l.append(dat)
-            for _ in range(8):
-                train_labels_l.append(d[i][1])
-        else:  # test data
-            test_images.append(d[i][0])
-            test_labels.append(d[i][1])
-    # shuffling
+    dat_transformers = [
+        lambda x: x,
+        lambda x: np.rot90(x, k=1, axes=(0, 1)),
+        lambda x: np.rot90(x, k=2, axes=(0, 1)),
+        lambda x: np.rot90(x, k=3, axes=(0, 1)),
+        lambda x: np.flip(x, axis=0),
+        lambda x: np.rot90(np.flip(x, axis=0), k=1, axes=(0, 1)),
+        lambda x: np.rot90(np.flip(x, axis=0), k=2, axes=(0, 1)),
+        lambda x: np.rot90(np.flip(x, axis=0), k=3, axes=(0, 1)),
+    ]
+
+    for i in range(len(d)):
+        dat = d[i][0]
+        for transf in dat_transformers:
+            train_images_l.append(transf(dat))
+            train_labels_l.append(d[i][1])
     train_images = np.array(train_images_l)
     train_labels = np.array(train_labels_l)
-    training_data = np.c_[train_images.reshape(
-        len(train_images), -1), train_labels.reshape(len(train_labels), -1)]
-    np.random.shuffle(training_data)
-    train_images2 = training_data[:, :train_images.size //
-                                  len(train_images)].reshape(
-                                      train_images.shape)
-    train_labels2 = training_data[:, train_images.size //
-                                  len(train_images):].reshape(
-                                      train_labels.shape)
+    # shuffling
+    # training_data = np.c_[train_images.reshape(
+    #     len(train_images), -1), train_labels.reshape(len(train_labels), -1)]
+    # np.random.shuffle(training_data)
+    # train_images2 = training_data[:, :train_images.size //
+    #                               len(train_images)].reshape(
+    #                                   train_images.shape)
+    # train_labels2 = training_data[:, train_images.size //
+    #                               len(train_images):].reshape(
+    #                                   train_labels.shape)
 
-    CONV3D_1_LAYERS = 16
+    CONV3D_1_FILTERS = 4
     # model
     model = Sequential([
-        Conv3D(CONV3D_1_LAYERS, 4, padding='same', activation='relu',
+        Conv3D(CONV3D_1_FILTERS, 2, padding='same', activation='relu',
                input_shape=(IMG_SIZE, IMG_SIZE, IMG_DEPTH_T, IMG_DEPTH_FRAMES)),
-        Dropout(0.2),
-        Reshape((IMG_SIZE, IMG_SIZE, IMG_DEPTH_T * CONV3D_1_LAYERS)),
-        Conv2D(32, 4, padding='same', activation='relu'),
         Flatten(),
         Dense(64, activation='relu'),
-        Dropout(0.5),
-        Dense(32, activation='relu'),
+        Dropout(0.4),
+        Dense(64, activation='relu'),
+        Dropout(0.4),
+        Dense(64, activation='relu'),
+        Dropout(0.2),
+        Dense(64, activation='relu'),
         Dense(1, activation='sigmoid')
     ])
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
+    model.compile(loss='binary_crossentropy',
                   metrics=['accuracy'])
     model.summary()
 
     # train
-    history = model.fit([train_images2], train_labels2,
-                        validation_split=0.1, epochs=8, batch_size=1024)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                                  patience=5, min_lr=0.001)
+    history = model.fit([train_images], train_labels, callbacks=[reduce_lr],
+                        validation_split=0.1, epochs=32, batch_size=2048)
 
     # test
     # test_loss, test_acc = model.evaluate([test_images], test_labels)
