64033a99f517f7b3476f2a523149760eb92dae0f
Christian Henkel <post@henkelchristian.de>
2021-01-21 17:56:35 +0100

    asterisk includes are well worth it here

--- scenarios/generators_test.py
+++ scenarios/generators_test.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-import scenarios.generators
+from scenarios.generators import *
 
 
 class TestGenerators(unittest.TestCase):
@@ -15,14 +15,14 @@ class TestGenerators(unittest.TestCase):
     def test_generate_random_gridmap(self):
         w = 999
         h = 100
-        gridmap_empty = scenarios.generators.generate_random_gridmap(w, h, 0)
+        gridmap_empty = generate_random_gridmap(w, h, 0)
 
         assert gridmap_empty.shape[0] == w
         assert gridmap_empty.shape[1] == h
         assert np.max(gridmap_empty) == 0
         assert np.min(gridmap_empty) == 0
 
-        gridmap_half = scenarios.generators.generate_random_gridmap(w, h, 0.5)
+        gridmap_half = generate_random_gridmap(w, h, 0.5)
 
         assert gridmap_half.shape[0] == w
         assert gridmap_half.shape[1] == h
@@ -31,14 +31,14 @@ class TestGenerators(unittest.TestCase):
 
     def test_generate_like_sim_decentralized_determinism(self):
         (base_env, base_starts, base_goals
-         ) = scenarios.generators.like_sim_decentralized(
+         ) = like_sim_decentralized(
             10, .5, 10, 0, ignore_cache=True
         )
         # --------
 
         # everything the same
         (same_env, same_starts, same_goals
-         ) = scenarios.generators.like_sim_decentralized(
+         ) = like_sim_decentralized(
             10, .5, 10, 0, ignore_cache=True
         )
         self.assertTrue(np.all(base_env == same_env))
@@ -48,7 +48,7 @@ class TestGenerators(unittest.TestCase):
 
         # everything different
         (other_env, other_starts, other_goals
-         ) = scenarios.generators.like_sim_decentralized(
+         ) = like_sim_decentralized(
             10, .5, 10, 1, ignore_cache=True
         )
         self.assertFalse(np.all(base_env == other_env))
@@ -58,7 +58,7 @@ class TestGenerators(unittest.TestCase):
 
         # only env different -> all different
         (other_env, other_starts, other_goals
-         ) = scenarios.generators.like_sim_decentralized(
+         ) = like_sim_decentralized(
             10, .4, 10, 0, ignore_cache=True
         )
         self.assertFalse(np.all(base_env == other_env))
@@ -67,14 +67,14 @@ class TestGenerators(unittest.TestCase):
 
     def test_generate_like_policylearn_gen_determinism(self):
         (base_env, base_starts, base_goals
-         ) = scenarios.generators.like_policylearn_gen(
+         ) = like_policylearn_gen(
             10, .5, 10, 0, ignore_cache=True
         )
         # --------
 
         # everything the same
         (same_env, same_starts, same_goals
-         ) = scenarios.generators.like_policylearn_gen(
+         ) = like_policylearn_gen(
             10, .5, 10, 0, ignore_cache=True
         )
         self.assertTrue(np.all(base_env == same_env))
@@ -84,7 +84,7 @@ class TestGenerators(unittest.TestCase):
 
         # everything different
         (other_env, other_starts, other_goals
-         ) = scenarios.generators.like_policylearn_gen(
+         ) = like_policylearn_gen(
             10, .5, 10, 1, ignore_cache=True
         )
         self.assertFalse(np.all(base_env == other_env))
@@ -94,7 +94,7 @@ class TestGenerators(unittest.TestCase):
 
         # only env different -> all different
         (other_env, other_starts, other_goals
-         ) = scenarios.generators.like_policylearn_gen(
+         ) = like_policylearn_gen(
             10, .4, 10, 0, ignore_cache=True
         )
         self.assertFalse(np.all(base_env == other_env))
@@ -104,34 +104,34 @@ class TestGenerators(unittest.TestCase):
     def test_generate_like_policylearn_gen_low_fills(self):
         """tests if generator handles low fill numbers correctly"""
         (env, starts, goals
-         ) = scenarios.generators.like_policylearn_gen(
+         ) = like_policylearn_gen(
             10, .1, 10, 0, ignore_cache=True
         )
         self.assertEqual(np.count_nonzero(env), 10)  # 10% of 10*10
 
         (env, starts, goals
-         ) = scenarios.generators.like_policylearn_gen(
+         ) = like_policylearn_gen(
             10, 0, 10, 0, ignore_cache=True
         )
         self.assertEqual(np.count_nonzero(env), 0)  # 0% of 10*10
 
     def test_get_random_next_to_free_pose_or_any_if_full(self):
-        env = scenarios.generators.generate_random_gridmap(2, 2, 0)
-        pos = scenarios.generators.get_random_next_to_free_pose_or_any_if_full(
+        env = generate_random_gridmap(2, 2, 0)
+        pos = get_random_next_to_free_pose_or_any_if_full(
             env)
         self.assertIn(pos[0], [0, 1])
         self.assertIn(pos[1], [0, 1])
 
     def test_tracing_pathes_in_the_dark_determinism(self):
         (base_env, base_starts, base_goals
-         ) = scenarios.generators.tracing_pathes_in_the_dark(
+         ) = tracing_pathes_in_the_dark(
             10, .5, 10, 0
         )
         # --------
 
         # everything the same
         (same_env, same_starts, same_goals
-         ) = scenarios.generators.tracing_pathes_in_the_dark(
+         ) = tracing_pathes_in_the_dark(
             10, .5, 10, 0
         )
         self.assertTrue(np.all(base_env == same_env))
@@ -140,7 +140,7 @@ class TestGenerators(unittest.TestCase):
 
         # everything different
         (other_env, other_starts, other_goals
-         ) = scenarios.generators.tracing_pathes_in_the_dark(
+         ) = tracing_pathes_in_the_dark(
             10, .5, 10, 1
         )
         self.assertFalse(np.all(base_env == other_env))
@@ -150,7 +150,7 @@ class TestGenerators(unittest.TestCase):
 
         # only env different -> all different
         (other_env, other_starts, other_goals
-         ) = scenarios.generators.tracing_pathes_in_the_dark(
+         ) = tracing_pathes_in_the_dark(
             10, .4, 10, 0
         )
         self.assertFalse(np.all(base_env == other_env))
@@ -160,13 +160,13 @@ class TestGenerators(unittest.TestCase):
     def test_tracing_pathes_in_the_dark_gen_low_fills(self):
         """tests if generator handles low fill numbers correctly"""
         (env, starts, goals
-         ) = scenarios.generators.tracing_pathes_in_the_dark(
+         ) = tracing_pathes_in_the_dark(
             10, .1, 10, 0
         )
         self.assertEqual(np.count_nonzero(env), 10)  # 10% of 10*10
 
         (env, starts, goals
-         ) = scenarios.generators.tracing_pathes_in_the_dark(
+         ) = tracing_pathes_in_the_dark(
             10, 0, 10, 0
         )
         self.assertEqual(np.count_nonzero(env), 0)  # 0% of 10*10
@@ -175,7 +175,7 @@ class TestGenerators(unittest.TestCase):
     def test_movingai_read_mapfile(self):
         for mapname in self.MAPS_WE_CARE_ABOUT.keys():
             mapfile = 'scenarios/movingai/mapf-map/' + mapname + '.map'
-            env = scenarios.generators.movingai_read_mapfile(mapfile)
+            env = movingai_read_mapfile(mapfile)
             assert len(env.shape) == 2
             assert env.shape[0] == self.MAPS_WE_CARE_ABOUT[mapname][0]
             assert env.shape[1] == self.MAPS_WE_CARE_ABOUT[mapname][1]
@@ -183,7 +183,7 @@ class TestGenerators(unittest.TestCase):
     def test_movingai_loading_maps(self):
         for mapfile in self.MAPS_WE_CARE_ABOUT:
             (env, starts, goals
-             ) = scenarios.generators.movingai(mapfile, "even", 0, 100)
+             ) = movingai(mapfile, "even", 0, 100)
 
 
 if __name__ == "__main__":  # pragma: no cover
