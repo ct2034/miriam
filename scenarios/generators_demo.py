9f765c008726583d63106027551f8cf97b0f2338
Christian Henkel <post@henkelchristian.de>
2021-01-28 17:20:25 +0100

    a jupyter notebook for fun

--- scenarios/generators_demo.py
+++ scenarios/generators_demo.py
@@ -1,37 +1,18 @@
 #!/usr/bin/env python3
-
-from matplotlib import pyplot as plt
-import numpy as np
-
 from scenarios.generators import movingai, tracing_pathes_in_the_dark
+from visualization import plot_with_arrows
 
 
 def demo_movingai():
     n_agents = 25
     env, starts, goals = movingai("Paris_1_256", "even", 0, n_agents)
-    plot(env, starts, goals)
+    plot_with_arrows(env, starts, goals)
 
 
 def demo_tracing_pathes_in_the_dark():
     n_agents = 3
     env, starts, goals = tracing_pathes_in_the_dark(50, .5, n_agents, 0)
-    plot(env, starts, goals)
-
-
-def plot(env, starts, goals):
-    plt.imshow(np.swapaxes(env, 0, 1), cmap='Greys', origin='lower')
-    n_agents = len(starts)
-    for i_a in range(n_agents):
-        plt.arrow(
-            starts[i_a][0] + .5,
-            starts[i_a][1] + .5,
-            goals[i_a][0] - starts[i_a][0],
-            goals[i_a][1] - starts[i_a][1],
-            width=1,
-            length_includes_head=True,
-            linewidth=0
-        )
-    plt.show()
+    plot_with_arrows(env, starts, goals)
 
 
 if __name__ == "__main__":
---
+++ scenarios/visualization.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def plot_env(ax, env):
+    ax.imshow(np.swapaxes(env, 0, 1), cmap='Greys', origin='lower')
+
+
+def plot_with_arrows(env, starts, goals):
+    fig = plt.figure(figsize=[5, 5])
+    ax = fig.add_subplot()
+    plot_env(ax, env)
+    n_agents = len(starts)
+    cmap = plt.get_cmap('hsv')
+    colors = [cmap(i) for i in np.linspace(0, 1, n_agents+1)]
+    for i_a in range(n_agents):
+        ax.arrow(
+            starts[i_a][0] + .5,
+            starts[i_a][1] + .5,
+            goals[i_a][0] - starts[i_a][0],
+            goals[i_a][1] - starts[i_a][1],
+            width=1,
+            length_includes_head=True,
+            linewidth=0,
+            color=colors[i_a])
+    fig.show()
