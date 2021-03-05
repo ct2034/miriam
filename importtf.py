93b84561d0053d40de40dbd3acf935e3b4ad25d2
Christian Henkel <post@henkelchristian.de>
2021-03-05 22:48:50 +0100

    nopep8 (:

--- importtf.py
+++ importtf.py
@@ -1,5 +1,8 @@
-import os
+import os  # nopep8
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
-from tensorflow import keras
-import tensorflow as tf
+# nopep8
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}  # nopep8
+import tensorflow as tf  # nopep8
+from tensorflow import keras  # nopep8
+
+# nopep8
