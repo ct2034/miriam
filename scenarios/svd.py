199c733b8f531aaeaf870af3363ee72dde5c022a
Christian Henkel <post@henkelchristian.de>
2021-05-21 10:10:01 +0200

    new data, new plots, less svd

--- scenarios/svd.py
+++ scenarios/svd.py
@@ -19,138 +19,134 @@ if __name__ == "__main__":
     print(df_results.shape)
 
     ignored_evaluations = [
-        "n_edges",
-        "n_edges_ta",
-        "n_nodes",
-        "n_nodes_ta"
+        # "n_edges",
+        # "n_edges_ta",
+        # "n_nodes",
+        # "n_nodes_ta"
     ]
 
     # changing dataframe ######################################################
-    if "svd" in args.pkl_read:  # was already transformed
-        df_svd = df_results
-        df_svd.sort_index(inplace=True)
-        new_idx = list(set(df_svd.index))
-        new_idx.sort()
-        print(new_idx)
-    else:  # transforming dataframe into more columns
-        idx = df_results.index
-        print(idx.names)
-        new_idx = list(set(idx.get_level_values(
-            idx.names[-1])))
-        print(new_idx)
-        new_idx += [idx.names[1]]  # fills
-        new_idx += [idx.names[2]]  # n_agents
-        print(new_idx)
-        df_svd = pd.DataFrame(index=new_idx)
-        print(df_svd.head())
-        idx.sort_values()
-
-        pb = ProgressBar("columns", len(df_results.columns), 10)
-        for col in df_results.columns:
-            for i in idx:
-                # print(i)
-                new_col = col+"_"+"_".join(map(str, i[:-1]))
-                # print(new_col)
-                df_svd.loc[i[-1], new_col] = df_results.loc[i, col]
-                df_svd.loc[idx.names[1], new_col] = i[1]  # fills
-                df_svd.loc[idx.names[2], new_col] = i[2]  # n_agents
-            pb.progress()
-        pb.end()
-        print(df_svd.head())
-        print(df_svd.shape)
-        df_svd.to_pickle("svd_"+args.pkl_read)
-
-    # sns.heatmap(df_svd)
-    # plt.show()
+    df_results.sort_index(inplace=True)
+    cols = list(df_results.columns)
+    cols.sort()
+    print(cols)
 
     # dropping ignored rows ###################################################
-    df_svd.drop(labels=ignored_evaluations, axis=0, inplace=True)
+    df_results_clean = df_results.drop(labels=ignored_evaluations, axis=1)
     for ev in ignored_evaluations:
-        new_idx.pop(new_idx.index(ev))
+        cols.pop(cols.index(ev))
 
     # preparation #############################################################
-    print("Dropping all nan rows ...")
-    df_svd.dropna(axis=0, how='all', inplace=True)
-    # print(df_svd.head())
-    print(df_svd.shape)
-    # sns.heatmap(df_svd)
+    print("Dropping any nan rows ...")
+    df_results_clean.dropna(axis=0, how='any', inplace=True)
+    # print(df_results_clean.head())
+    print(df_results_clean.shape)
+    # sns.heatmap(df_results_clean)
     # plt.show()
 
-    print("Dropping any nan cols ...")
-    df_svd.dropna(axis=1, how='any', inplace=True)
-    # print(df_svd.head())
-    print(df_svd.shape)
-    # sns.heatmap(df_svd)
-    # plt.show()
-
-    df_svd_scaled = (df_svd-df_svd.mean())/df_svd.std()
-    # print(df_svd_scaled.head())
+    first_std = df_results_clean.std()
+    for c in cols:
+        if c in first_std.index and first_std[c] == 0:
+            df_results_clean.drop(columns=[c], inplace=True)
+        elif c not in first_std.index:  # non numeric column
+            df_results_clean.drop(columns=[c], inplace=True)
+    df_results_clean = df_results_clean.apply(pd.to_numeric)
+    numeric_var_cols = df_results_clean.columns
+    print(numeric_var_cols)
+    mean = df_results_clean.mean()
+    std = df_results_clean.std()
+    df_results_scaled = (df_results_clean-mean) / std
+    print(df_results_scaled.head())
 
     # svd #####################################################################
-    U, S, V = np.linalg.svd(df_svd_scaled, full_matrices=False)
+    U, S, V = np.linalg.svd(df_results_scaled, full_matrices=False)
 
     # analyzing explained variance ############################################
     var_explained = S/np.sum(S)
-    var_explained = var_explained[:20]
+    var_explained = var_explained[:len(cols)]
 
     plt.figure()
     sns.barplot(x=list(range(1, len(var_explained)+1)),
                 y=var_explained)
     plt.xlabel('SVs', fontsize=16)
     plt.ylabel('Percent Variance Explained', fontsize=16)
+    plt.savefig("scenarios/plots/variance_explained.png")
 
     # visualizing matrices ####################################################
     plt.figure()
-    sns.heatmap(U, cmap="RdBu")
+    sns.heatmap(U[:len(numeric_var_cols), :], cmap="RdBu")
     plt.title("U")
     plt.yticks(
-        list(map(lambda x: x+.5, range(len(new_idx)))),
-        new_idx,
+        list(map(lambda x: x+.5, range(len(numeric_var_cols)))),
+        numeric_var_cols,
         rotation='horizontal')
-    plt.savefig("heatmap_u.png")
+    plt.savefig("scenarios/plots/heatmap_u.png")
 
     plt.figure()
-    sns.heatmap(V[:, :20], cmap="RdBu")
+    sns.heatmap(V, cmap="RdBu")
     plt.title("V")
+    plt.savefig("scenarios/plots/heatmap_v.png")
+
+    # what a (over)view #######################################################
+    sns.pairplot(
+        df_results,
+        hue="generator",
+        markers=["o", "s", "D"],
+        plot_kws={"s": 3},
+        x_vars=[
+            "ecbs_cost",
+            "icts_cost",
+            "diff_indep",
+            "diff_sim_decen_learned",
+            # "difference_sim_decen_minus_learned"
+        ],
+        y_vars=[
+            "mean_degree",
+            "n_nodes_ta",
+            "n_edges_ta",
+            "bridges",
+            "connectivity",
+            "tree_width",
+            "uncentrality"
+        ],
+    )
+    plt.savefig("scenarios/plots/pairplot.png")
 
     # is there something? #####################################################
-    plt.figure()
-    df_svd_t = df_svd.transpose()
-    sns.scatterplot(
-        x='mean_degree',
-        y='fills',
-        hue='ecbs_cost',
-        data=df_svd_t,
-        palette="viridis",
-        s=10)
+    # plt.figure()
+    # sns.scatterplot(
+    #     x='mean_degree',
+    #     y='fill',
+    #     hue='ecbs_cost',
+    #     data=df_results,
+    #     palette="viridis",
+    #     s=10)
 
-    plt.figure()
-    df_svd_t = df_svd.transpose()
-    sns.scatterplot(
-        x='mean_degree',
-        y='diff_indep',
-        hue='ecbs_cost',
-        data=df_svd_t,
-        palette="viridis",
-        s=10)
+    # plt.figure()
+    # sns.scatterplot(
+    #     x='mean_degree',
+    #     y='diff_indep',
+    #     hue='ecbs_cost',
+    #     data=df_results,
+    #     palette="viridis",
+    #     s=10)
 
-    plt.figure()
-    df_svd_t = df_svd.transpose()
-    sns.scatterplot(
-        x='n_agentss',
-        y='diff_indep',
-        hue='mean_degree',
-        data=df_svd_t,
-        palette="viridis",
-        s=10)
+    # plt.figure()
+    # sns.scatterplot(
+    #     x='n_agents',
+    #     y='diff_indep',
+    #     hue='mean_degree',
+    #     data=df_results,
+    #     palette="viridis",
+    #     s=10)
 
-    plt.figure()
-    df_svd_t = df_svd.transpose()
-    sns.scatterplot(
-        x='mean_degree',
-        y='diff_sim_decen_learned',
-        hue='n_agentss',
-        data=df_svd_t,
-        palette="viridis",
-        s=10)
-    plt.show()
+    # plt.figure()
+    # sns.scatterplot(
+    #     x='mean_degree',
+    #     y='diff_sim_decen_learned',
+    #     hue='n_agents',
+    #     data=df_results,
+    #     palette="viridis",
+    #     s=10)
+
+    # plt.show()
