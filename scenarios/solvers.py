4f65e6deb24799925af5a58163f2f6059fcd08cf
Christian Henkel <post@henkelchristian.de>
2021-06-06 20:00:45 +0200

    worst error

--- scenarios/solvers.py
+++ scenarios/solvers.py
@@ -99,7 +99,7 @@ def to_agent_objects(env, starts, goals, policy=PolicyType.RANDOM):
     n_agents = starts.shape[0]
     agents = []
     for i_a in range(n_agents):
-        a = Agent(env, starts[i_a])
+        a = Agent(env, starts[i_a], policy=policy)
         if not a.give_a_goal(goals[i_a]):
             return INVALID
         agents.append(a)
@@ -147,4 +147,4 @@ def decentralized(env, starts, goals, policy: PolicyType):
     if agents is INVALID:
         return INVALID
     return run_a_scenario(
-        env, agents, plot=False, iterator=IteratorType.BLOCKING1)
+        env, agents, plot=False, iterator=IteratorType.BLOCKING3)
