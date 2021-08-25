27cf6de63d3a81f37a4df61ee573d2fff6ea3d7d
Christian Henkel <post@henkelchristian.de>
2021-08-25 12:01:30 +0200

    works now

--- roadmaps/var_odrm_torch/var_odrm_torch.py
+++ roadmaps/var_odrm_torch/var_odrm_torch.py
@@ -32,7 +32,7 @@ def make_graph(pos):
     return g
 
 
-def draw_graph(g, pos):
+def draw_graph(g, pos, paths=[]):
     plt.figure()
     N = pos.shape[0]
     pos_np = pos.detach().numpy()
@@ -42,10 +42,18 @@ def draw_graph(g, pos):
         "edgecolors": "black",
         "linewidths": 1,
         "width": 1,
-        "with_labels": False
+        # "with_labels": False
     }
     pos_dict = {i: pos_np[i] for i in range(N)}
     nx.draw_networkx(g, pos_dict, **options)
+
+    for p in paths:
+        for i in range(len(p)-1):
+            j = i+1
+            plt.plot(
+                [pos_np[p[i], 0], pos_np[p[j], 0]],  # x
+                [pos_np[p[i], 1], pos_np[p[j], 1]],  # y
+                'y--')
 
 
 def make_paths(g, n_paths, seed=random.randint(0, 10E6)):
@@ -55,7 +63,7 @@ def make_paths(g, n_paths, seed=random.randint(0, 10E6)):
     for _ in range(n_paths):
         start, goal = np.random.randint(0, N, (2))
         paths.append(
-            nx.shortest_path(g, start, goal))
+            nx.shortest_path(g, start, goal, 'distance'))
     return paths
 
 
@@ -63,7 +71,7 @@ def get_path_len(pos, path):
     sections = torch.zeros(len(path)-1)
     for i in range(len(path)-1):
         j = i+1
-        sections[i] = torch.linalg.vector_norm(pos[i] - pos[j])
+        sections[i] = torch.linalg.vector_norm(pos[path[i]] - pos[path[j]])
     return torch.sum(sections)
 
 
@@ -81,23 +89,26 @@ if __name__ == "__main__":
 
     pos = sample_points(n)
     g = make_graph(pos)
-    draw_graph(g, pos)
 
+    optimizer = torch.optim.Adam([pos], lr=learning_rate)
     test_paths = make_paths(g, 20, 0)
     test_costs = []
+
+    draw_graph(g, pos, [test_paths[0]])
     for i_b in range(batches):
+        optimizer.zero_grad()
         test_length = get_paths_len(pos, test_paths)
         paths = make_paths(g, 10, i_b)
         length = get_paths_len(pos, paths)
-        backward = length.backward(retain_graph=True)
-        pos = torch.tensor(pos - pos.grad * learning_rate, requires_grad=True)
+        backward = length.backward(create_graph=True)
+        optimizer.step()
         g = make_graph(pos)
-        if i_b % 10 == 0:
+        if i_b % 100 == 0:
             print(f"test_length: {test_length}")
             print(f"length: {length}")
             test_costs.append(test_length.detach().numpy())
 
-    draw_graph(g, pos)
+    draw_graph(g, pos, [test_paths[0]])
 
     plt.figure()
     plt.plot(test_costs)
