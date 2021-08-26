a12c43b936ec2abe143ba844fe9b9eda0bf01fb9
Christian Henkel <post@henkelchristian.de>
2021-08-26 08:38:33 +0200

    shrinking and display fixed

--- roadmaps/var_odrm_torch/var_odrm_torch.py
+++ roadmaps/var_odrm_torch/var_odrm_torch.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import torch
+from networkx.exception import NetworkXNoPath, NodeNotFound
+from pyflann import FLANN
 from scipy.spatial import Delaunay
+from tools import ProgressBar
 
 dtype = torch.float
 device = torch.device("cpu")
@@ -20,96 +23,150 @@ def make_graph(pos):
     N = pos.shape[0]
     g = nx.Graph()
     pos_np = pos.detach().numpy()
-
+    # to discourage too many edges parallel to walls,
+    # these will be ignored after Delaunay because they are not added by any
+    # edge below.
+    n_fn = 5
+    fake_nodes = np.array(
+        [(0, 1/n_fn*i) for i in range(n_fn+1)] +
+        [(1, 1/n_fn*i) for i in range(n_fn+1)] +
+        [(1/n_fn*i, 0) for i in range(n_fn+1)] +
+        [(1/n_fn*i, 1) for i in range(n_fn+1)]
+    )
+    pos_np = np.append(pos_np, fake_nodes, axis=0)
     tri = Delaunay(pos_np)
     (indptr, indices) = tri.vertex_neighbor_vertices
     for i in range(N):
         neighbors = indices[indptr[i]:indptr[i+1]]
         for n in neighbors:
+            if n >= N:
+                continue  # ignoring fake nodes form above
             g.add_edge(i, n,
                        distance=np.linalg.norm(pos_np[i] - pos_np[n]))
-
     return g
 
 
 def draw_graph(g, pos, paths=[]):
-    plt.figure()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
     N = pos.shape[0]
     pos_np = pos.detach().numpy()
     options = {
+        "ax": ax,
         "node_size": 20,
         "node_color": "red",
         "edgecolors": "black",
         "linewidths": 1,
         "width": 1,
-        # "with_labels": False
+        "with_labels": False
     }
     pos_dict = {i: pos_np[i] for i in range(N)}
     nx.draw_networkx(g, pos_dict, **options)
 
+    style = 'y--'
     for p in paths:
-        for i in range(len(p)-1):
+        (start, goal, path_vs) = p
+        for i in range(len(path_vs)-1):
             j = i+1
-            plt.plot(
-                [pos_np[p[i], 0], pos_np[p[j], 0]],  # x
-                [pos_np[p[i], 1], pos_np[p[j], 1]],  # y
-                'y--')
+            ax.plot(
+                [pos_np[path_vs[i], 0], pos_np[path_vs[j], 0]],  # x
+                [pos_np[path_vs[i], 1], pos_np[path_vs[j], 1]],  # y
+                style)
+        ax.plot(
+            [start[0], pos_np[path_vs[0], 0]],  # x
+            [start[1], pos_np[path_vs[0], 1]],  # y
+            style)
+        ax.plot(
+            [goal[0], pos_np[path_vs[-1], 0]],  # x
+            [goal[1], pos_np[path_vs[-1], 1]],  # y
+            style)
+        ax.set_xlim([0., 1.])
+        ax.set_ylim([0., 1.])
 
 
-def make_paths(g, n_paths, seed=random.randint(0, 10E6)):
+def make_paths(g, pos, n_paths, seed=random.randint(0, 10E6)):
+    nn = 1
     np.random.seed(seed)
-    N = nx.number_of_nodes(g)
+    pos_np = pos.detach().numpy()
+    flann = FLANN()
+    flann.build_index(np.array(pos_np))
     paths = []
     for _ in range(n_paths):
-        start, goal = np.random.randint(0, N, (2))
-        paths.append(
-            nx.shortest_path(g, start, goal, 'distance'))
+        [start, goal] = np.random.random((2, 2))
+        result, dists = flann.nn_index(
+            np.array([start, goal], dtype=pos_np.dtype), nn)
+        start_v, goal_v = result
+        try:
+            paths.append(
+                (start, goal, nx.shortest_path(g, start_v, goal_v, 'distance')))
+        except (NetworkXNoPath, nx.NodeNotFound):
+            pass
     return paths
 
 
 def get_path_len(pos, path):
-    sections = torch.zeros(len(path)-1)
-    for i in range(len(path)-1):
+    TAIL_WEIGHT = 4
+    (start, goal, path_vs) = path
+    sections = torch.zeros(len(path_vs)+1)
+    for i in range(len(path_vs)-1):
         j = i+1
-        sections[i] = torch.linalg.vector_norm(pos[path[i]] - pos[path[j]])
+        sections[i] = torch.linalg.vector_norm(
+            pos[path_vs[i]] - pos[path_vs[j]])
+    s_tail = torch.linalg.vector_norm(
+        torch.tensor(start) - pos[path_vs[0]])
+    sections[-2] = TAIL_WEIGHT * (s_tail ** 2 + s_tail)
+    g_tail = torch.linalg.vector_norm(
+        torch.tensor(goal) - pos[path_vs[-1]])
+    sections[-1] = TAIL_WEIGHT * (g_tail ** 2 + g_tail)
     return torch.sum(sections)
 
 
 def get_paths_len(pos, paths):
     all_lens = torch.zeros(len(paths))
     for i, p in enumerate(paths):
-        all_lens[i] = get_path_len(pos, p)
+        try:
+            all_lens[i] = get_path_len(pos, p)
+        except (NetworkXNoPath, NodeNotFound):
+            pass
     return torch.sum(all_lens)
 
 
 if __name__ == "__main__":
     n = 100
-    batches = 2000
-    learning_rate = 1e-4
+    batches = 1000
+    learning_rate = 1e-3
+    stats_every = 10
 
     pos = sample_points(n)
     g = make_graph(pos)
+    test_paths = make_paths(g, pos, 20, 0)
 
     optimizer = torch.optim.Adam([pos], lr=learning_rate)
-    test_paths = make_paths(g, 20, 0)
     test_costs = []
 
     draw_graph(g, pos, [test_paths[0]])
+    plt.savefig("pre_training.png")
+    pb = ProgressBar("Training", batches)
     for i_b in range(batches):
         optimizer.zero_grad()
+        test_paths = make_paths(g, pos, 20, 0)
         test_length = get_paths_len(pos, test_paths)
-        paths = make_paths(g, 10, i_b)
-        length = get_paths_len(pos, paths)
-        backward = length.backward(create_graph=True)
+        training_paths = make_paths(g, pos, 10, i_b)
+        training_length = get_paths_len(pos, training_paths)
+        backward = training_length.backward(create_graph=True)
         optimizer.step()
         g = make_graph(pos)
-        if i_b % 100 == 0:
+        if i_b % stats_every == stats_every-1:
             print(f"test_length: {test_length}")
-            print(f"length: {length}")
+            print(f"training_length: {training_length}")
             test_costs.append(test_length.detach().numpy())
+            pb.progress(i_b)
+    pb.end()
 
     draw_graph(g, pos, [test_paths[0]])
+    plt.savefig("post_training.png")
 
     plt.figure()
     plt.plot(test_costs)
+    plt.savefig("test_length.png")
     plt.show()
