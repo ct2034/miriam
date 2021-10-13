96a1844d1b8dae9b4c263d5a715c3dab002f1208
Christian Henkel <post@henkelchristian.de>
2021-10-13 18:14:29 +0200

    evaluation at the end

--- planner/mapf_with_rl/gym_mapf_demo.py
+++ planner/mapf_with_rl/gym_mapf_demo.py
@@ -1,18 +1,18 @@
 import gym
 from gym.spaces import Discrete
 from planner.mapf_with_rl.gym_mapf_gcn_policy import GcnPolicy
+from planner.mapf_with_rl.mapf_with_rl import evaluate, make_useful_scenarios
 from stable_baselines3 import DQN
 from stable_baselines3.dqn.policies import MlpPolicy
 
 env = gym.make("gym_mapf:mapf-v0")
 
 model = DQN(GcnPolicy, env, verbose=1)
-model.learn(total_timesteps=10000, log_interval=4)
+model.learn(total_timesteps=10000, log_interval=100)
 
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-        obs = env.reset()
+# testing this
+test_scenarios = make_useful_scenarios(100, 0, True, 6, 4)
+assert isinstance(model.policy, GcnPolicy)
+qfun = model.policy.q_net
+evaluate(test_scenarios, qfun, True, False)
+evaluate(test_scenarios, qfun, True, True)
