471d27d7f73c39286730fd81b28156bee101ffea
Christian Henkel <post@henkelchristian.de>
2021-10-29 22:38:55 +0200

    bigger figure

--- planner/mapf_with_rl/mapf_with_rl_plot.py
+++ planner/mapf_with_rl/mapf_with_rl_plot.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import json
+import sys
 from typing import Dict, List, Tuple
 
 from matplotlib import pyplot as plt
 
 
 def plot_this_experiment(data: Dict[str, Tuple[List[int], List[float]]]):
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+    plt.style.use('bmh')
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(14.4, 10.8))
     for name, (x, y) in data.items():
         if "success" in name:
             ax = ax2
@@ -17,7 +19,7 @@ def plot_this_experiment(data: Dict[str, Tuple[List[int], List[float]]]):
             ax.set_ylim(bottom=-1)
             ax.spines.bottom.set_position('zero')
             ax.spines.top.set_color('none')
-        ax.plot(x, y, label=name, linewidth=.5)
+        ax.plot(x, y, label=name, linewidth=1)
     for ax in [ax1, ax2, ax3]:
         ax.legend(loc='lower left')
 
@@ -27,3 +29,9 @@ def make_plot_from_json(name: str):
         data = json.load(f)
     plot_this_experiment(data)
     plt.savefig(f'planner/mapf_with_rl/results/{name}.png', dpi=300)
+
+
+if __name__ == '__main__':
+    # make plot from command line arguments
+    if len(sys.argv) > 1:
+        make_plot_from_json(sys.argv[1])
