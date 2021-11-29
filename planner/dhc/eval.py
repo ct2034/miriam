07f804c3992f6155eb46820559e90159865e11bf
Christian Henkel <post@henkelchristian.de>
2021-11-29 10:29:10 +0100

    using ghc

---
+++ planner/dhc/eval.py
@@ -0,0 +1,23 @@
+from random import Random
+
+from planner.dhc import generate_scenarios
+from planner.dhc.DHC import test
+from scenarios.generators import tracing_pathes_in_the_dark
+
+
+def eval(env, starts, goals):
+    fname = generate_scenarios.generate_file((env, starts, goals))
+    res = test.test_model(337500, fname)
+    return res
+
+
+if __name__ == "__main__":
+    rng = Random(0)
+    size = 10
+    fill = .5
+    n_agents = 5
+    env, starts, goals = tracing_pathes_in_the_dark(
+        size, fill, n_agents, rng)
+
+    res = eval(env, starts, goals)
+    print(res)
---
+++ planner/dhc/generate_scenarios.py
@@ -0,0 +1,39 @@
+import pickle
+from random import Random
+
+from definitions import SCENARIO_TYPE
+from scenarios.generators import tracing_pathes_in_the_dark
+from tools import hasher
+from tqdm import tqdm
+
+
+def generate_file(scen: SCENARIO_TYPE):
+    """
+    Generate a file for Scenario scen.
+    :param scen: The type of scenario to generate.
+    """
+    hash = hasher(scen)
+    fname = f"planner/dhc/DHC/test_set/{hash}.pth"
+    tests = [scen]
+    with open(fname, 'wb') as f:
+        pickle.dump(tests, f)
+    return fname
+
+
+if __name__ == "__main__":
+    n_scenarios = 10
+    tests = []
+    rng = Random(0)
+
+    size = 10
+    fill = .5
+    n_agents = 5
+
+    for _ in tqdm(range(n_scenarios)):
+        env, starts, goals = tracing_pathes_in_the_dark(
+            size, fill, n_agents, rng)
+        tests.append((env, starts, goals))
+
+    path = f'./DHC/test_set/tracing_pathes_in_the_dark_{size}_{fill}_{n_agents}.pth'
+    with open(path, 'wb') as f:
+        pickle.dump(tests, f)
