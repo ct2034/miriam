b8a3bb4dc42ea558efbbf348cdf1b3988748121f
Christian Henkel <post@henkelchristian.de>
2021-12-03 14:52:04 +0100

    test case for ecbs

--- planner/mapf_implementations/plan_ecbs_test.py
+++ planner/mapf_implementations/plan_ecbs_test.py
@@ -1,9 +1,12 @@
+import logging
 import unittest
+from random import Random
 
 import numpy as np
 from definitions import INVALID
 from planner.mapf_implementations.plan_ecbs import plan_in_gridmap
 from scenarios import test_helper
+from scenarios.generators import arena_with_crossing
 
 
 class PlanEcbsTest(unittest.TestCase):
@@ -20,7 +23,7 @@ class PlanEcbsTest(unittest.TestCase):
             ],
             suboptimality=1.5,
             timeout=10,
-            disappear_at_goal=True  # can be solved 
+            disappear_at_goal=True  # can be solved
         )
         self.assertNotEqual(res, INVALID)
 
@@ -40,3 +43,22 @@ class PlanEcbsTest(unittest.TestCase):
             disappear_at_goal=False  # can not be solved then
         )
         self.assertEqual(res, INVALID)
+
+    def test_a_scenario(self):
+        rng = Random(0)
+        for _ in range(10):
+            env, s, g = arena_with_crossing(4, 0.5, 3, rng)
+            res = plan_in_gridmap(
+                gridmap=env,
+                starts=s,
+                goals=g,
+                suboptimality=1.5,
+                timeout=10,
+                disappear_at_goal=False  # can not be solved then
+            )
+            self.assertNotEqual(res, INVALID)
+
+
+if __name__ == '__main__':
+    logging.getLogger("sim.decentralized.runner").setLevel(logging.DEBUG)
+    PlanEcbsTest().test_a_scenario()
---
+++ planner/mapf_implementations/test_cases/d951c050-542c-11ec-a185-8546fc76ecba.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+
+./libMultiRobotPlanning/build/ecbs -i test_cases/d951c050-542c-11ec-a185-8546fc76ecba_in.yaml -o test_cases/d951c050-542c-11ec-a185-8546fc76ecba_out.yaml -w 1.5
