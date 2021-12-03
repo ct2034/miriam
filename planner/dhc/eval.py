0f0e0ededd79563feb3dacbfc00284feee7331be
Christian Henkel <post@henkelchristian.de>
2021-12-03 12:17:58 +0100

    cleanup of scenario files but keeping one for testing

--- planner/dhc/eval.py
+++ planner/dhc/eval.py
@@ -1,3 +1,4 @@
+import os
 from random import Random
 
 from planner.dhc import generate_scenarios
@@ -8,6 +9,8 @@ from scenarios.generators import tracing_pathes_in_the_dark
 def eval(env, starts, goals):
     fname = generate_scenarios.generate_file((env, starts, goals))
     res = test.test_model(337500, fname)
+    if os.path.exists(fname):
+        os.remove(fname)
     return res
 
 
