1cf016e1cc1676fe89322028f9f6ac5a9dd0aea2
Christian Henkel <post@henkelchristian.de>
2021-12-03 12:20:00 +0100

    this is bag of work

--- planner/primal2/eval.py
+++ planner/primal2/eval.py
@@ -7,17 +7,18 @@ import numpy as np
 from definitions import SCENARIO_TYPE
 from scenarios.evaluators import cost_ecbs
 from scenarios.generators import tracing_pathes_in_the_dark
+from tools import hasher
 
 
 def convert_scenario_to_primal(
-        scen: SCENARIO_TYPE, name: str):
+        scen: SCENARIO_TYPE, path: str):
     """
     Converts a scenario to primal format.
     :param scen: scenario
     :return: path to npy file
     """
     (env, starts, goals) = scen
-    cwd = os.path.dirname(__file__)
+    workdir = os.path.dirname(__file__)
 
     # this should contain a
     #   -1 for every obstacle
@@ -44,44 +45,66 @@ def convert_scenario_to_primal(
         out_goals[tuple(goal)] = i + 1
 
     info = np.array([out_state, out_goals])
-    fname = f"{cwd}/scenarios/{name}.npy"
-    np.save(fname, info)
+    np.save(path, info)
+
+
+def get_fnames(workdir, scen):
+    name = str(hasher(scen))
+    scenario_fname = f"{workdir}/scenarios/{name}.npy"
+    results_fname = f"{workdir}/testing_result/{name}_oneshotPRIMAL2.txt"
+    return scenario_fname, results_fname
+
+
+def eval_primal2(workdir):
+    try:
+        process = subprocess.Popen(
+            ["./run.bash"],
+            cwd=workdir,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True)
+        output, error = process.communicate()
+        print(f"error: {error.decode('utf-8')}")
+        print(f"output: {output.decode('utf-8')}")
+    except Exception as e:
+        print(f"error: {e}")
 
 
 if __name__ == '__main__':
-    cwd = os.path.dirname(__file__)
+    workdir = os.path.dirname(__file__)
+    n_eval = 10
+    # generate files
     rng = Random(0)
-    for _ in range(10):
+    scens = []
+    for _ in range(n_eval):
         scen = tracing_pathes_in_the_dark(
             size=8,
             fill=.5,
             n_agents=4,
             rng=rng
         )
-        print(f"cost_ecbs: {cost_ecbs(*scen)}")
-        name = 'demo'
-        try:
-            convert_scenario_to_primal(scen, name)
-            process = subprocess.Popen(
-                ["./run.bash"],
-                cwd=cwd,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                shell=True)
-            output, error = process.communicate()
-            # print(f"error: {error}")
-            print(f"output: {output}")
-            # reading results:
-            with open(f"{cwd}/testing_result/{name}_oneshotPRIMAL2.txt", "r") as f:
-                lines = f.readlines()
-                print("results:")
-                for line in lines:
-                    print(line)
-        except Exception as e:
-            print(f"error: {e}")
-        finally:
-            # if file exists, delete it
-            if os.path.isfile(f"{cwd}/scenarios/{name}.npy"):
-                os.remove(f"{cwd}/scenarios/{name}.npy")
-            if os.path.isfile(f"{cwd}/testing_result/{name}_oneshotPRIMAL2.txt"):
-                os.remove(f"{cwd}/testing_result/{name}_oneshotPRIMAL2.txt")
+        scens.append(scen)
+        convert_scenario_to_primal(scen, get_fnames(workdir, scen)[0])
+
+    # # run primal2
+    # eval_primal2(workdir)
+
+    # # read results
+    # count_results = 0
+    # for scen in scens:
+    #     scenario_fname, results_fname = get_fnames(workdir, scen)
+    #     if os.path.isfile(results_fname):
+    #         # with open(results_fname, "r") as f:
+    #         #     lines = f.readlines()
+    #         #     for line in lines:
+    #         #         print(line)
+    #         count_results += 1
+    #     else:
+    #         print("no file")
+
+    #     # clean up
+    #     if os.path.isfile(scenario_fname):
+    #         os.remove(scenario_fname)
+    #     if os.path.isfile(results_fname):
+    #         os.remove(results_fname)
+    # print(f"{count_results}/{n_eval} scenarios evaluated")
--- planner/primal2/run.bash
+++ planner/primal2/run.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo pwd
+echo $(pwd)
 source ./primally/bin/activate
+ls -lh ./scenarios
 python PRIMAL2/TestGenerator.py -a True
-python PRIMAL2/TestingEnv.py
+ls -lh ./scenarios
+# python PRIMAL2/TestingEnv.py
+python PRIMAL2/MultiProcessingTestDriver.py
