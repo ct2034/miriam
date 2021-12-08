ae34bebbf62ed42b5e916cf4d10852c5e1eaeb05
Christian Henkel <post@henkelchristian.de>
2021-12-08 16:49:12 +0100

    moving cache to a better place

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 # source miriam_env/bin/activate
 export PYTHONPATH=${PYTHONPATH}:$(pwd)
-export SCENARIO_STORAGE_PATH=planner/policylearn/scenario_cache
+export SCENARIO_STORAGE_PATH=.scenario_cache
