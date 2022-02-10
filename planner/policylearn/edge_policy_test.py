43e519e5786a425386258993daac10baec4d50db
ct2034 <post@henkelchristian.de>
2022-02-10 23:52:10 +0100

    actual learning in testcas

--- planner/policylearn/edge_policy_test.py
+++ planner/policylearn/edge_policy_test.py
@@ -25,32 +25,39 @@ class EdgePolicyTest(unittest.TestCase):
         self.assertEqual(score.shape, (4,))
         self.assertEqual(targets.shape, (4,))
 
-    @unittest.skip(reason="lets make this work properly, first")
     def test_edge_policy_learn(self):
         rng = Random(0)
-        num_node_features = 1
-        conv_channels = 1
+        torch.manual_seed(0)
+        num_node_features = 2
+        conv_channels = 3
         n_nodes = 2
         n_epochs = 10
         n_data = 10
         policy = EdgePolicyModel(num_node_features, conv_channels)
-        optimizer = torch.optim.SGD(policy.parameters(), lr=.1)
+        optimizer = torch.optim.Adam(policy.parameters(), lr=.1)
         datas = []
 
         # test data
         big_from_small = {n: n for n in range(n_nodes)}
-        test_data = self.make_data(rng, num_node_features, n_nodes)
-        pred = policy.predict(
-            test_data.x, test_data.edge_index, big_from_small)
-        self.assertNotEqual(pred, test_data.y)
+        test_data = [self.make_data(
+            rng, num_node_features, n_nodes) for _ in range(10)]
+        test_acc = policy.accuracy(
+            test_data, [big_from_small] * len(test_data))
+        self.assertLess(test_acc, 0.6)
 
         # train
         for _ in range(n_epochs):
             for _ in range(n_data):
                 datas.append(self.make_data(rng, num_node_features, n_nodes))
             policy.learn(datas, optimizer)
-            print(policy.accuracy(datas, [big_from_small] * len(datas)))
-        self.assertEqual(pred, test_data.y)
+            test_acc = policy.accuracy(
+                test_data, [big_from_small] * len(test_data))
+            print(test_acc)
+
+        # accuracy after training must have increased
+        test_acc = policy.accuracy(
+            test_data, [big_from_small] * len(test_data))
+        self.assertGreater(test_acc, 0.85)
 
     def make_data(self, rng, num_node_features, n_nodes):
         node = rng.randint(0, n_nodes-1)
