307dd55d751caf7e87b9ef7da22fc94194a6e139
Christian Henkel <post@henkelchristian.de>
2022-03-06 20:56:29 +0100

    the converter handles data that is numpy

--- planner/astar_boost/converter.py
+++ planner/astar_boost/converter.py
@@ -6,7 +6,7 @@ def initialize_from_graph(g):
     pos = nx.get_node_attributes(g, 'pos')
     posl = [[0., 0.]] * (max(pos.keys()) + 1)
     for k, v in pos.items():
-        posl[k] = list(v)
-    edges = [[e[0], e[1]] for e in g.edges()]
+        posl[k] = v.tolist()
+    edges = [[int(e[0]), int(e[1])] for e in g.edges()]
     a = AstarSolver(posl, edges)
     return a
