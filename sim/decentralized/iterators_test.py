3e6a3bd2b9b8a8815187dc94cbe79da32f84a82a
Christian Henkel <post@henkelchristian.de>
2022-03-09 21:37:21 +0100

    testing the motion collision function

--- sim/decentralized/iterators_test.py
+++ sim/decentralized/iterators_test.py
@@ -19,6 +19,13 @@ class TestIterators(unittest.TestCase):
         self.agents[1].give_a_goal((0, 0))
         self.agents[2].give_a_goal((0, 0))
 
+        # a graph for collision checking
+        self.g = nx.Graph()
+        self.g.add_edges_from([(0, 1), (0, 2), (1, 2)])
+        self.g.add_node(0, pos=(0., 0.))
+        self.g.add_node(1, pos=(0., 1.))
+        self.g.add_node(2, pos=(1., 0.))
+
     def test_get_possible_next_agent_poses_when_all_are_allowed(self):
         # getting next steps when all are allowed
         possible_next_agent_poses = get_possible_next_agent_poses(
@@ -68,6 +75,72 @@ class TestIterators(unittest.TestCase):
         self.assertListEqual(list(edge_colissions.keys()), [edge_in_col])
         self.assertIn(0, edge_colissions[edge_in_col])
         self.assertIn(1, edge_colissions[edge_in_col])
+
+    def test_check_motion_col(self):
+        # no colissions, radius too small
+        starts = [0, 1, 2]
+        ends = [1, 2, 0]
+        colliding_agents = check_motion_col(self.g, 0.22, starts, ends)
+        self.assertEqual(len(colliding_agents), 0)
+
+        # all collisions on bigger radius
+        colliding_agents = check_motion_col(self.g, 0.23, starts, ends)
+        self.assertEqual(len(colliding_agents), 3)
+        self.assertIn(0, colliding_agents)
+        self.assertIn(1, colliding_agents)
+        self.assertIn(2, colliding_agents)
+
+        # looking at only the outer edges
+        # no colissions, radius too small
+        starts = [0, 1]
+        ends = [2, 0]
+        colliding_agents = check_motion_col(self.g, 0.35, starts, ends)
+        self.assertEqual(len(colliding_agents), 0)
+
+        # all collisions on bigger radius
+        colliding_agents = check_motion_col(self.g, 0.36, starts, ends)
+        self.assertEqual(len(colliding_agents), 2)
+        self.assertIn(0, colliding_agents)
+        self.assertIn(1, colliding_agents)
+
+        # moving to the same node should always collide
+        all_nodes = [0, 1, 2]
+        for node in all_nodes:
+            starts = [node, node]
+            ends = [n for n in all_nodes if n != node]
+            colliding_agents = check_motion_col(self.g, 0.01, starts, ends)
+            self.assertEqual(len(colliding_agents), 2)
+            self.assertIn(0, colliding_agents)
+            self.assertIn(1, colliding_agents)
+
+        # waiting should be possible
+        # and lead to no colission when radius is small
+        starts = [0, 1]
+        ends = [0, 2]
+        colliding_agents = check_motion_col(self.g, 0.35, starts, ends)
+        self.assertEqual(len(colliding_agents), 0)
+
+        # and lead to colission when radius is big
+        colliding_agents = check_motion_col(self.g, 0.36, starts, ends)
+        self.assertEqual(len(colliding_agents), 2)
+        self.assertIn(0, colliding_agents)
+        self.assertIn(1, colliding_agents)
+
+        # if one agent doesn't collide ...
+        # the other two should
+        starts = [0, 1, 2]
+        ends = [0, 2, 1]
+        colliding_agents = check_motion_col(self.g, 0.35, starts, ends)
+        self.assertEqual(len(colliding_agents), 2)
+        self.assertIn(1, colliding_agents)
+        self.assertIn(2, colliding_agents)
+
+        # if radius is bigger, all collide
+        colliding_agents = check_motion_col(self.g, 0.36, starts, ends)
+        self.assertEqual(len(colliding_agents), 3)
+        self.assertIn(0, colliding_agents)
+        self.assertIn(1, colliding_agents)
+        self.assertIn(2, colliding_agents)
 
 
 if __name__ == "__main__":  # pragma: no cover
