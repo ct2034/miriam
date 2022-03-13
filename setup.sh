f3d393700a2b5ec048d175677d6023297926c0f8
Christian Henkel <post@henkelchristian.de>
2022-03-13 15:32:36 +0100

    this helps ...

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 export PYTHONPATH=${PYTHONPATH}:$(pwd)
 export SCENARIO_STORAGE_PATH=.scenario_cache
 export LD_LIBRARY_PATH=/usr/local/cuda-11.3/targets/x86_64-linux/lib:$LD_LIBRARY_PATH
+ulimit -n 10000
