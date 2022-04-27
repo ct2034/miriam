f7472803ec254bafce951a31d8f6472939260b2c
Christian Henkel <post@henkelchristian.de>
2022-04-27 23:42:37 +0200

    reading the paths

--- planner/bvc/plan_bvc.py
+++ planner/bvc/plan_bvc.py
@@ -1,8 +1,10 @@
 import json
 import os
+from functools import reduce
+from typing import List
 
 import numpy as np
-from definitions import MAP_IMG
+from definitions import INVALID, MAP_IMG
 from matplotlib import pyplot as plt
 from tools import hasher, run_command
 
@@ -10,6 +12,7 @@ SCENARIOS_FOLDER = '.scenarios_cache'
 JSON_TEMPLATE_FOLDER = 'json_templates'
 ROBOT_X_FNAME = 'robot_x.json'
 TWOD_CONFIG_FNAME = '2d_config.json'
+STATISTICS_FNAME = 'statistics.json'
 STR_GOAL_POS = "goal_position"
 STR_RADIUS = "radius"
 STR_START_POS = "start_position"
@@ -19,6 +22,7 @@ STR_FRAMES = "frames"
 STR_ROBOT_POSITIONS = "robot_positions"
 STR_ROBOT_ID = "robot_id"
 STR_POSITION = "position"
+STR_PLANNING_FAIL = "planning_fail"
 
 
 def get_scenario_folder(hash: str = ""):
@@ -26,6 +30,31 @@ def get_scenario_folder(hash: str = ""):
     Get the folder where the scenarios are stored.
     """
     return os.path.join(os.path.dirname(__file__), SCENARIOS_FOLDER, hash)
+
+
+def merge_paths_s(paths_s_in: List[np.ndarray]) -> np.ndarray:
+    """
+    Merge paths_s into one set of paths.
+    """
+    paths_s = [paths_s_in[0]]
+    for i_p in range(1, len(paths_s_in)):
+        next_paths = paths_s_in[i_p]
+        prev_paths = paths_s[-1]
+        i_n = 0
+        i_p = 0
+        while (next_paths[:, i_n] != prev_paths[:, i_p]).any():
+            i_p += 1
+        pass
+
+    paths = np.concatenate(paths_s_in, axis=1)
+    return paths
+
+
+def get_average_path_length(paths: np.ndarray) -> float:
+    """
+    Get the average path length of a set of paths.
+    """
+    return np.mean(np.sum(np.abs(paths[1:] - paths[:-1]), axis=0))
 
 
 def plan(map_img: MAP_IMG, starts, goals, radius: float):
@@ -82,8 +111,16 @@ def plan(map_img: MAP_IMG, starts, goals, radius: float):
     print(f"{stdout=}")
     print(f"{stderr=}")
 
+    # Check if successful.
+    with open(os.path.join(scenario_folder, STATISTICS_FNAME), 'r') as f:
+        content = f.readlines()
+    assert content is not None
+    lines = reduce(lambda x, y: x + y, content)
+    if STR_PLANNING_FAIL in lines:
+        return INVALID
+
     # Read the paths.
-    f, ax = plt.subplots()
+    paths_s = []
     for file in os.listdir(scenario_folder):
         if file.startswith("vis") and file.endswith(".json"):
             paths = [list() for _ in range(n_agents)]
@@ -95,13 +132,17 @@ def plan(map_img: MAP_IMG, starts, goals, radius: float):
                             position[STR_POSITION])
             os.remove(os.path.join(scenario_folder, file))
             paths_np = np.array(paths)
-            for i_a in range(n_agents):
-                ax.plot(
-                    paths_np[i_a, :, 0],
-                    paths_np[i_a, :, 1],
-                    label=f"robot_{i_a} {file}")
-    ax.legend()
-    ax.set_aspect('equal')
-    plt.show()
+            paths_s.append(paths_np)
+    all_paths = merge_paths_s(paths_s)
+    return all_paths
 
-    # TODO: Check if successful.
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection='3d')
+    # for i_a in range(n_agents):
+    #     ax.plot(
+    #         all_paths[i_a, :, 0],
+    #         all_paths[i_a, :, 1],
+    #         range(all_paths.shape[1]),
+    #         label=f"robot_{i_a}")
+    # ax.legend()
+    # plt.show()
--- planner/bvc/plan_bvc_demo.py
+++ planner/bvc/plan_bvc_demo.py
@@ -1,12 +1,16 @@
-from planner.bvc.plan_bvc import plan
+import numpy as np
+from planner.bvc.plan_bvc import get_average_path_length, plan
 from scenarios.visualization import plot_with_paths
 
 if __name__ == "__main__":
     map_img = ((0,),)
-    starts = [[0.3, 0.2], [0.6, 0.2], [0.4, 0.8], [0.7, 0.8]]
-    goals = [[0.6, 0.8], [0.3, 0.8], [0.8, 0.3], [0.4, 0.3]]
+    starts = [[0.3, 0.2], [0.6, 0.2], [0.4, 0.8]]
+    goals = [[0.6, 0.8], [0.3, 0.8], [0.8, 0.3]]
 
     paths = plan(map_img, starts, goals, radius=0.05)
+    assert isinstance(paths, np.ndarray)
+    avg_len = get_average_path_length(paths)
+    print(f"Average path length: {avg_len}")
 
     print(paths)
     plot_with_paths(map_img, paths)
