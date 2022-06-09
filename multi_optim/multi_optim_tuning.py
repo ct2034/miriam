16c4d5cd9b41015a8db3ddbfee531d3cd45b35ee
Christian Henkel <post@henkelchristian.de>
2022-06-09 18:38:25 +0200

    not testing with 256 epochs

--- multi_optim/multi_optim_tuning.py
+++ multi_optim/multi_optim_tuning.py
@@ -41,7 +41,7 @@ def params_run():
         "n_nodes": [64],  # medium
         "n_runs_pose": [128],
         "n_runs_policy": [128],
-        "n_epochs_per_run_policy":  [128, 256],
+        "n_epochs_per_run_policy":  [128],
         "batch_size_policy":  [64],
         "stats_and_eval_every": [4],
         "lr_pos": [1E-3, 1E-2, 1E-3],
