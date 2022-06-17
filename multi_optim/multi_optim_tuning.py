6fd20e56f8969641a60846f677e9b06e9cca6fdb
Christian Henkel <post@henkelchristian.de>
2022-06-17 18:17:41 +0200

    tuning for different epoch nrs

--- multi_optim/multi_optim_tuning.py
+++ multi_optim/multi_optim_tuning.py
@@ -41,7 +41,8 @@ def params_run():
         "n_nodes": [64],  # medium
         "n_runs_pose": [128],
         "n_runs_policy": [128],
-        "n_epochs_per_run_policy":  [128],
+        "n_epochs_per_run_policy":  [1, 2, 4, 6, 8],
+        "n_episodes_per_run_policy":  [256],
         "batch_size_policy":  [64],
         "stats_and_eval_every": [4],
         "lr_pos": [1E-3, 1E-2, 1E-3],
