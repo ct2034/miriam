fd9ba736a9a592c3be9b2d452510d08029835ce7
Christian Henkel <post@henkelchristian.de>
2022-07-11 16:58:54 +0200

    running all again

--- multi_optim/multi_optim_run.py
+++ multi_optim/multi_optim_run.py
@@ -600,71 +600,71 @@ if __name__ == "__main__":
         prefix=prefix,
         pool_in=pool)
 
-    # # tiny run
-    # prefix = "tiny"
-    # logging.getLogger(__name__).setLevel(logging.INFO)
-    # logging.getLogger(
-    #     "planner.mapf_implementations.plan_cbs_roadmap"
-    # ).setLevel(logging.INFO)
-    # run_optimization(
-    #     n_nodes=16,
-    #     n_runs_pose=64,
-    #     n_runs_policy=64,
-    #     n_episodes_per_run_policy=256,
-    #     n_epochs_per_run_policy=4,
-    #     batch_size_policy=128,
-    #     stats_and_eval_every=2,
-    #     lr_pos=1e-3,
-    #     lr_policy=1e-3,
-    #     max_n_agents=4,
-    #     map_fname="roadmaps/odrm/odrm_eval/maps/x.png",
-    #     seed=0,
-    #     prefix=prefix,
-    #     pool_in = pool)
+    # tiny run
+    prefix = "tiny"
+    logging.getLogger(__name__).setLevel(logging.INFO)
+    logging.getLogger(
+        "planner.mapf_implementations.plan_cbs_roadmap"
+    ).setLevel(logging.INFO)
+    run_optimization(
+        n_nodes=16,
+        n_runs_pose=64,
+        n_runs_policy=64,
+        n_episodes_per_run_policy=256,
+        n_epochs_per_run_policy=4,
+        batch_size_policy=128,
+        stats_and_eval_every=2,
+        lr_pos=1e-3,
+        lr_policy=1e-3,
+        max_n_agents=4,
+        map_fname="roadmaps/odrm/odrm_eval/maps/x.png",
+        seed=0,
+        prefix=prefix,
+        pool_in = pool)
 
-    # # small run
-    # prefix = "small"
-    # logging.getLogger(__name__).setLevel(logging.INFO)
-    # logging.getLogger(
-    #     "planner.mapf_implementations.plan_cbs_roadmap"
-    # ).setLevel(logging.INFO)
-    # run_optimization(
-    #     n_nodes=32,
-    #     n_runs_pose=64,
-    #     n_runs_policy=64,
-    #     n_episodes_per_run_policy=256,
-    #     n_epochs_per_run_policy=4,
-    #     batch_size_policy=128,
-    #     stats_and_eval_every=2,
-    #     lr_pos=1e-3,
-    #     lr_policy=3e-4,
-    #     max_n_agents=6,
-    #     map_fname="roadmaps/odrm/odrm_eval/maps/x.png",
-    #     seed=0,
-    #     prefix=prefix,
-    #     pool_in=pool)
+    # small run
+    prefix = "small"
+    logging.getLogger(__name__).setLevel(logging.INFO)
+    logging.getLogger(
+        "planner.mapf_implementations.plan_cbs_roadmap"
+    ).setLevel(logging.INFO)
+    run_optimization(
+        n_nodes=32,
+        n_runs_pose=64,
+        n_runs_policy=64,
+        n_episodes_per_run_policy=256,
+        n_epochs_per_run_policy=4,
+        batch_size_policy=128,
+        stats_and_eval_every=2,
+        lr_pos=1e-3,
+        lr_policy=3e-4,
+        max_n_agents=6,
+        map_fname="roadmaps/odrm/odrm_eval/maps/x.png",
+        seed=0,
+        prefix=prefix,
+        pool_in=pool)
 
-    # # medium run
-    # prefix = "medium"
-    # logging.getLogger(__name__).setLevel(logging.INFO)
-    # logging.getLogger(
-    #     "planner.mapf_implementations.plan_cbs_roadmap"
-    # ).setLevel(logging.INFO)
-    # run_optimization(
-    #     n_nodes=64,
-    #     n_runs_pose=64,
-    #     n_runs_policy=64,
-    #     n_episodes_per_run_policy=256,
-    #     n_epochs_per_run_policy=4,
-    #     batch_size_policy=128,
-    #     stats_and_eval_every=2,
-    #     lr_pos=1e-3,
-    #     lr_policy=1e-4,
-    #     max_n_agents=10,
-    #     map_fname="roadmaps/odrm/odrm_eval/maps/x.png",
-    #     seed=0,
-    #     prefix=prefix,
-    #     pool_in=pool)
+    # medium run
+    prefix = "medium"
+    logging.getLogger(__name__).setLevel(logging.INFO)
+    logging.getLogger(
+        "planner.mapf_implementations.plan_cbs_roadmap"
+    ).setLevel(logging.INFO)
+    run_optimization(
+        n_nodes=64,
+        n_runs_pose=64,
+        n_runs_policy=64,
+        n_episodes_per_run_policy=256,
+        n_epochs_per_run_policy=4,
+        batch_size_policy=128,
+        stats_and_eval_every=2,
+        lr_pos=1e-3,
+        lr_policy=1e-4,
+        max_n_agents=10,
+        map_fname="roadmaps/odrm/odrm_eval/maps/x.png",
+        seed=0,
+        prefix=prefix,
+        pool_in=pool)
 
     # large run
     prefix = "large"
