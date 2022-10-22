12b41775034a52806517ccea74df340b338d87dc
Christian Henkel <post@henkelchristian.de>
2022-10-22 13:26:27 +0200

    this is boxplots now

--- multi_optim/multi_optim_separate.py
+++ multi_optim/multi_optim_separate.py
@@ -116,19 +116,20 @@ def run_separately(
 
 
 def plot(save_folder: str, prefix_s: List[str]):
-    titles = ["both", "only roadmap", "only policy"]
-    metrics = ["path_length", "policy_regret"]  # , "overall_success"]
+    titles = ["only roadmap", "only policy"]
+    metrics = ["path_length", "general_success", "general_regret"]
     width = .8
 
     n_nodes_per_prefix = {}
     n_agents_per_prefix = {}
     stats_per_prefix = {}
+    both_stats_per_prefix = {}
     for prefix in prefix_s:
         # we need three files:
-        stats_per_prefix[prefix] = []
-        stats_per_prefix[prefix].append(yaml.load(
+        both_stats_per_prefix[prefix] = yaml.load(
             open(f"{save_folder}/{prefix}_stats.yaml", "r"),
-            yaml.SafeLoader))
+            yaml.SafeLoader)
+        stats_per_prefix[prefix] = []
         stats_per_prefix[prefix].append(yaml.load(
             open(f"{save_folder}/{prefix}_sep_roadmap_stats.yaml", "r"),
             yaml.SafeLoader))
@@ -160,43 +161,69 @@ def plot(save_folder: str, prefix_s: List[str]):
         prefix_s, key=lambda x: n_nodes_per_prefix[x])
 
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']  # type: ignore
-    fig, axs = plt.subplots(1, len(metrics), figsize=(8, 4.5))
-    assert isinstance(axs, np.ndarray)
+    f_width = 4.5 * len(metrics)
+    fig, axs = plt.subplots(1, len(metrics), figsize=(f_width, 4.5))
+    axs = [axs] if len(metrics) == 1 else axs
+    # assert isinstance(axs, np.ndarray)
     for i_m, metric in enumerate(metrics):
         tick_labels = []
         for i_p, prefix in enumerate(prefixes_sorted_by_n_nodes):
             n_nodes = n_nodes_per_prefix[prefix]
             tick_labels.append(f"{n_nodes} ({n_agents_per_prefix[prefix]})")
             n_agents = n_agents_per_prefix[prefix]
+            both_stats = both_stats_per_prefix[prefix]
             # less agents here for less noise in data
             # less_n_agents = n_agents - 2 if n_agents > 2 else n_agents
             if metric == "path_length":
                 key = f"general_length_{n_agents}"
             elif metric == "policy_regret":
                 key = f"policy_regret_{n_agents}"
+            elif metric == "general_regret":
+                key = f"general_regret_{n_agents}"
             elif metric == "overall_success":
                 key = f"general_success_{n_agents}"
+            elif metric == "policy_success":
+                key = f"policy_success_{n_agents}"
             assert key is not None
             for i_s, stat in enumerate(stats_per_prefix[prefix]):
-                if i_p == 0:
-                    title = titles[i_s]
-                else:
-                    title = None
-                axs[i_m].bar(
-                    i_p + (i_s-1) * width / len(titles),
-                    abs(stat[key]['x'][-1]),
-                    width=width / len(titles),
-                    label=title,
-                    color=colors[i_s])
+                stat_end = len(stat[key]['x'])
+                stat_start = int(stat_end * 0.8)
+                both_stats_end = len(both_stats[key]['x'])
+                both_stats_start = both_stats_end - stat_end + stat_start
+                data = (
+                    np.array(stat[key]['x'])[stat_start:stat_end-1] -
+                    np.array(both_stats[key]['x'])[
+                        both_stats_start:both_stats_end-1]
+                )
+                elements = axs[i_m].boxplot(
+                    [data],
+                    positions=[i_p + (i_s-.5) * width / len(titles)],
+                    widths=[width / len(titles)],
+                    labels=[stat])
+                for el in elements['boxes']:
+                    el.set_color(colors[i_s+2])
+                for el in elements['medians']:
+                    el.set_color(colors[i_s+2])
+                for el in elements['whiskers']:
+                    el.set_color(colors[i_s+2])
+                for el in elements['caps']:
+                    el.set_color(colors[i_s+2])
+                for el in elements['fliers']:
+                    el.set_markeredgecolor(colors[i_s+2])
         pretty_name = " ".join(
             map(lambda x: x.capitalize(), metric.split("_")))
-        axs[i_m].set_title(pretty_name)
+        axs[i_m].plot(-5, 0, color=colors[2],
+                      label='roadmap only - both')  # for legend
+        axs[i_m].plot(-5, 0, color=colors[3],
+                      label='policy only - both')  # for legend
+        axs[i_m].set_xlim(-.5, len(n_agents_per_prefix)-.5)
+        # axs[i_m].set_title("Path Length Surplus")
         axs[i_m].set_xlabel("Number of Vertices (Agents)")
         axs[i_m].set_xticks(range(len(prefix_s)))
         axs[i_m].set_xticklabels(tick_labels)
-        axs[i_m].set_ylabel(pretty_name)
+        axs[i_m].set_ylabel(pretty_name + " Difference Ablation - Baseline")
 
-    axs[0].legend(loc='lower left')
+    axs[0].legend(loc='upper right')
 
     fig.tight_layout()
     plt.savefig(f"{save_folder}/ablation.pdf")
@@ -210,7 +237,7 @@ if __name__ == "__main__":
 
     prefix_s = [
         # "debug",
-        "tiny",
+        # "tiny",
         "small",
         "medium",
         "large"
