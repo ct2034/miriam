40a86fffacc64510565358ab0d54e753a06ee76a
Christian Henkel <post@henkelchristian.de>
2024-11-02 18:29:33 +0100

    better setup
    Signed-off-by: Christian Henkel <post@henkelchristian.de>

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 source miriam_env/bin/activate
 source .env
 ulimit -n 100000
+
+# this must be run from the root of the project
+if [ ! -f "setup.sh" ]; then
+    echo "Please run this script from the root of the project"
+    exit 1
+fi
+export PYTHONPATH=$PYTHONPATH:$(pwd)
